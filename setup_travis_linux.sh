49fdc206aca11381419458a7de86715dd24a2e71
PIERRE POLLASTRI <pollastri.p@gmail.com>
2016-11-02 10:45:38 +0100

    Update travis

--- setup_travis_linux.sh
+++ setup_travis_linux.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
-    echo "deb http://ftp.debian.com/debian/ unstable main" | cat - /etc/apt/source.list | sudo tee /etc/apt/source.list;
-    sudo add-apt-repository "deb http://ftp.debian.com/debian/ unstable main";
-    sudo apt-get update;
-    sudo apt-get -o Dpkg::Options::="--force-all" -y --force-yes install gcc-5 g++-5;
-    sudo cp /bin/true /usr/bin/dpkg-maintscript-helper;
-    sudo apt-get -o Dpkg::Options::="--force-all" -y --force-yes install mingw-w64;
-elif [["$TRAVIS_OS_NAME" == "osx" ]]; then
-   brew update;
-   brew upgrade cmake;
+if [ "$TRAVIS_OS_NAME" == "linux" ]; then
+    echo "deb http://ftp.debian.com/debian/ unstable main" | cat - /etc/apt/source.list | sudo tee /etc/apt/source.list
+    sudo add-apt-repository "deb http://ftp.debian.com/debian/ unstable main"
+    sudo apt-get update
+    sudo apt-get -o Dpkg::Options::="--force-all" -y --force-yes install gcc-5 g++-5
+    sudo cp /bin/true /usr/bin/dpkg-maintscript-helper
+    sudo apt-get -o Dpkg::Options::="--force-all" -y --force-yes install mingw-w64
+elif [ "$TRAVIS_OS_NAME" == "osx" ]; then
+   brew update
+   brew upgrade cmake
 fi;
