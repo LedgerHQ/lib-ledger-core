0cda111a0cc737ff3ffdca22680b8eb60b31662f
PIERRE POLLASTRI <pollastri.p@gmail.com>
2016-11-15 16:52:31 +0100

    Add implementation for wallet pool

---
+++ core/src/wallet/pool/WalletPoolBuilder.cpp
@@ -0,0 +1,86 @@
+/*
+ *
+ * WalletPoolBuilder
+ * ledger-core
+ *
+ * Created by Pierre Pollastri on 15/11/2016.
+ *
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2016 Ledger
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+#include "WalletPoolBuilder.hpp"
+#include "WalletPool.hpp"
+
+namespace ledger { namespace core {
+
+        WalletPoolBuilder::WalletPoolBuilder() {
+            _self = std::shared_ptr<api::WalletPoolBuilder>(this);
+        }
+
+        std::shared_ptr<api::WalletPoolBuilder>
+        WalletPoolBuilder::setHttpClient(const std::shared_ptr<api::HttpClient> &client) {
+            _httpClient = client;
+            return _self;
+        }
+
+        std::shared_ptr<api::WalletPoolBuilder>
+        WalletPoolBuilder::setWebsocketClient(const std::shared_ptr<api::WebSocketClient> &client) {
+            _webSocketClient = client;
+            return _self;
+        }
+
+        std::shared_ptr<api::WalletPoolBuilder>
+        WalletPoolBuilder::setPathResolver(const std::shared_ptr<api::PathResolver> &pathResolver) {
+            _pathResolver = pathResolver;
+            return _self;
+        }
+
+        std::shared_ptr<api::WalletPoolBuilder>
+        WalletPoolBuilder::setLogPrinter(const std::shared_ptr<api::LogPrinter> &printer) {
+            _logPrinter = printer;
+            return _self;
+        }
+
+        std::shared_ptr<api::WalletPoolBuilder>
+        WalletPoolBuilder::setThreadDispatcher(const std::shared_ptr<api::ThreadDispatcher> &dispatcher) {
+            _dispatcher = dispatcher;
+            return _self;
+        }
+
+        std::shared_ptr<api::WalletPoolBuilder> WalletPoolBuilder::setName(const std::string &name) {
+            _name = name;
+            return _self;
+        }
+
+        void WalletPoolBuilder::build(const std::shared_ptr<api::WalletPoolBuildCallback> &listener) {
+            _self = nullptr;
+            auto pool = std::make_shared<WalletPool>();
+        }
+
+        std::shared_ptr<api::WalletPoolBuilder> api::WalletPoolBuilder::createInstance() {
+            return std::shared_ptr<api::WalletPoolBuilder>(new ledger::core::WalletPoolBuilder());
+        }
+    }
+}
+
+
