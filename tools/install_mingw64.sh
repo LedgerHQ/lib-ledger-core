15dac5178d1386d1fa64cbf5278b2744aaf2cb4f
PIERRE POLLASTRI <pollastri.p@gmail.com>
2016-11-29 17:09:47 +0100

    Fix mingw

--- tools/install_mingw64.sh
+++ tools/install_mingw64.sh
@@ -4,5 +4,5 @@ mkdir tools/mingw64_tmp/ && cp tools/mingw64/* tools/mingw64_tmp/
 cd tools/mingw64_tmp
 for file in *.xz; do
     tar xf ${file}
+    sudo rsync -lrK usr /
 done
-sudo rsync -lrK usr /
