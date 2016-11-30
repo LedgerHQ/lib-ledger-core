79f01d8a2a448974e5f37c6cfc74c3a847579d0f
PIERRE POLLASTRI <pollastri.p@gmail.com>
2016-11-30 13:08:06 +0100

    download gcc

--- tools/install_mingw64.sh
+++ tools/install_mingw64.sh
@@ -2,8 +2,9 @@
 
 mkdir tools/mingw64_tmp/ && cp tools/mingw64/* tools/mingw64_tmp/
 cd tools/mingw64_tmp
+wget https://api.ledgerwallet.com/assets/sources/mingw-w64-gcc-6.2.1-1-x86_64.pkg.tar.xz
 for file in *.xz; do
-    tar xfv ${file}
+    tar xf ${file}
 done
 
-sudo rsync -vlrK usr /
+sudo rsync -lrK usr /git
