92a57c25f55a04b66885ef03981f73597f7d7298
KhalilBellakrid <khalil.bellakrid@ledger.fr>
2018-03-14 23:35:47 +0100

    add part of djinni files documentation (did not re-generate files), add CI integration on Linux

---
+++ .circleci/build_lib.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+echo "=====>Create build directory"
+pwd
+cd .. && mkdir lib-ledger-core-build && cd lib-ledger-core-build
+
+echo "=====>Start build"
+pwd
+cmake -DCMAKE_INSTALL_PREFIX=/root ../lib-ledger-core && make
