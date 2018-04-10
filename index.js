7daa44b6849d3c80443c0edfc6804900ecbc0be0
KhalilBellakrid <khalil.bellakrid@ledger.fr>
2018-04-10 13:39:26 +0200

    add a test app for react-native binding using react-native init command

--- index.js
+++
@@ -1,7 +0,0 @@
-const binding = require('bindings')('ledgerapp_nodejs'); // eslint-disable-line global-require
-/*
-    Test NJSSecp256k1 Implementation
- */
-const NJSSecp256k1Impl = new binding.NJSSecp256k1();
-const pubKeys = NJSSecp256k1Impl.computePubKey([123],true);
-console.log(pubKeys);
