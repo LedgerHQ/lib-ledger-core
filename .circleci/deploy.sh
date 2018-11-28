8355e8fd5dfcb683630dac1f15cacbc7982e8466
KhalilBellakrid <khalil.bellakrid@ledger.fr>
2018-11-28 05:20:01 +0100

    Fix job filtering on circleCI

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
-cd ../lib-ledger-core-artifacts
-aws s3 sync ./ s3://ledger-lib-ledger-core/$LIB_VERSION/ --acl public-read && \
-aws s3 ls s3://ledger-lib-ledger-core/$LIB_VERSION;
+#for tests
+if [ -n "$CIRCLE_TAG" ] && [ "$CIRCLE_BRANCH" == "ci-improvements" -o "$CIRCLE_BRANCH" == "develop" ]; then
+	cd ../lib-ledger-core-artifacts
+	ls -la
+	aws s3 sync ./ s3://ledger-lib-ledger-core/$LIB_VERSION/ --acl public-read && \
+	aws s3 ls s3://ledger-lib-ledger-core/$LIB_VERSION;
+fi
--- .circleci/deploy_fat_lib.sh
+++ .circleci/deploy_fat_lib.sh
@@ -1,22 +1,22 @@
 #!/usr/bin/env bash
 
 unamestr=$(uname)
-branchstr=$(git branch | grep '*' | sed 's/^..//')
 
-cd ios
+if [ "$unamestr" == "Darwin" ]; then
+	cd ios
 
-echo "======> Build Fat Library"
-pwd
+	echo "======> Build Fat Library"
+	pwd
 
-#We don't put x86_64/ledger-core.framework/ledger-core otherwise we have a problem when pushing to AppStore
-lipo -create armv7/ledger-core.framework/ledger-core arm64/ledger-core.framework/ledger-core -o ledger-core
-mkdir ledger-core.framework
-mv ledger-core ledger-core.framework/
-cp arm64/ledger-core.framework/Info.plist ledger-core.framework/
-install_name_tool -add_rpath "@executable_path/Frameworks/universal" ledger-core.framework/ledger-core
-lipo -info ledger-core.framework/ledger-core
+	#We don't put x86_64/ledger-core.framework/ledger-core otherwise we have a problem when pushing to AppStore
+	lipo -create armv7/ledger-core.framework/ledger-core arm64/ledger-core.framework/ledger-core -o ledger-core
+	mkdir ledger-core.framework
+	mv ledger-core ledger-core.framework/
+	cp arm64/ledger-core.framework/Info.plist ledger-core.framework/
+	install_name_tool -add_rpath "@executable_path/Frameworks/universal" ledger-core.framework/ledger-core
+	lipo -info ledger-core.framework/ledger-core
 
 
-aws s3 sync ./ s3://ledger-lib-ledger-core/$LIB_VERSION/ios/universal --acl public-read --exclude "x86_64/*" --exclude "armv7/*" --exclude "arm64/*" && \
-aws s3 ls s3://ledger-lib-ledger-core/$LIB_VERSION/ios/universal;
-
+	aws s3 sync ./ s3://ledger-lib-ledger-core/$LIB_VERSION/ios/universal --acl public-read --exclude "x86_64/*" --exclude "armv7/*" --exclude "arm64/*" && \
+	aws s3 ls s3://ledger-lib-ledger-core/$LIB_VERSION/ios/universal;
+fi
