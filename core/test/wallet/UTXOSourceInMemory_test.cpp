1973ba122b8d29cd64b4ed8d0ec0591622f6675c
Dimitri Sabadie <dimitri.sabadie@ledger.fr>
2018-12-28 17:54:56 +0100

    Add tests for UTXOSourceInMemory (initial).
    LLC-76

--- core/test/wallet/UTXOSourceInMemory_test.cpp
+++ core/test/wallet/UTXOSourceInMemory_test.cpp
@@ -1,28 +1,43 @@
+#include <CommonFixtureFunctions.hpp>
 #include <Mocks.hpp>
 #include <Helpers.hpp>
+#include <async/Future.hpp>
 #include <gtest/gtest.h>
 #include <memory>
+#include <wallet/common/InMemoryBlockchainDatabase.hpp>
 #include <wallet/bitcoin/UTXOSourceInMemory.hpp>
 
 using namespace ledger::core;
+using namespace ledger::core::common;
 using namespace ledger::core::bitcoin;
 using namespace ledger::core::tests;
+using namespace ::testing;
 
-struct UTXOSourceInMemoryTest: ::testing::Test {
+struct UTXOSourceInMemoryFixture: virtual Test, CommonFixtureFunctions {
 protected:
     std::shared_ptr<SimpleExecutionContext> _ctx;
-    UTXOSourceInMemory _source;
+    std::shared_ptr<NiceMock<BlocksDBMock>> _blocksDB;
+    std::shared_ptr<InMemoryBlockchainDatabase<BitcoinLikeNetwork::FilledBlock>> _fakeDB;
+    std::shared_ptr<UTXOSourceInMemory> _source;
 
 public:
-    UTXOSourceInMemoryTest()
-        : _ctx(new SimpleExecutionContext()),
-          _source(std::make_shared(BlocksDBMock),
-                  std::make_shared(KeychainRegistryMock)) {
+    UTXOSourceInMemoryFixture()
+        : _ctx(std::make_shared<SimpleExecutionContext>()),
+          _blocksDB(std::make_shared<NiceMock<BlocksDBMock>>()),
+          _fakeDB(std::make_shared<InMemoryBlockchainDatabase<BitcoinLikeNetwork::FilledBlock>>(_ctx)),
+          _source(std::make_shared<UTXOSourceInMemory>(
+              _blocksDB,
+              std::make_shared<KeychainRegistryMock>()
+          )) {
+        linkMockDbToFake(_blocksDB, *_fakeDB);
     }
-}
+};
 
-struct UTXOSourceInMemorySimpleTest: UTXOSourceInMemoryTest;
+TEST_F(UTXOSourceInMemoryFixture, NoInitialUTXO) {
+    auto future = _source->getUTXOs(_ctx);
+    _ctx->wait();
+    auto sourceList = getFutureResult(future);
 
-TEST_F(UTXOSourceInMemorySimpleTest, NoInitialUTXO) {
-    EXPECT_TRUE(false);
+    EXPECT_TRUE(sourceList.available.empty());
+    EXPECT_TRUE(sourceList.spent.empty());
 }
