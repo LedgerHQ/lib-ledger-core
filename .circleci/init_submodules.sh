01ba65aa4c2abd0e74dce427dc74d9fa9436a251
KhalilBellakrid <khalil.bellakrid@ledger.fr>
2018-12-31 13:43:23 +0100

    Add SQLCipher build to iOS CI

--- .circleci/init_submodules.sh
+++ .circleci/init_submodules.sh
@@ -24,3 +24,14 @@ cd $HOME/lib-ledger-core/core/lib/leveldb && git checkout bitcoin-fork
 echo "========> Generate ITFs"
 cd $HOME/lib-ledger-core
 ./tools/generate_interfaces.sh
+
+unamestr=`uname`
+#This is a workaround because building directly
+#SQLCipher for arm64 and armv7 is failing
+#fro more details please refer to comments
+#in core/lib/cmake/configure_sqlcipher_ios.sh
+if [ "$unamestr" == "Darwin" ]; then
+	echo "========> Generating SQLCipher for iOS"
+	./build_sqlcipher_ios_arm64_armv7.sh armv7
+	./build_sqlcipher_ios_arm64_armv7.sh arm64
+fi
