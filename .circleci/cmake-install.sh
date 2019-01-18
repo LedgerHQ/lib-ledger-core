515d4f7d0e66003b6a80943bde06fc905d5bbbc6
Andrii Korol <andrii.korol@ledger.fr>
2019-01-18 18:27:55 +0100

    Fix bitcoin_test initilization.
    The order of initialization of global data is not defined.
    So we can have currency = currencies::BITCOIN call before
    it been created.

--- .circleci/cmake-install.sh
+++ .circleci/cmake-install.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 echo "========> Install CMake"
 cd $HOME
-wget --quiet https://github.com/Kitware/CMake/releases/download/v3.13.3/cmake-3.13.3-Linux-x86_64.tar.gz && tar -xvf cmake-3.13.3-Linux-x86_64.tar.gz
+wget --quiet https://cmake.org/files/v3.12/cmake-3.12.3.tar.gz && tar -xvf cmake-3.12.3.tar.gz
+cd cmake-3.12.3
