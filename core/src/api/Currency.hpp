e88344a9be21eb18e33ea467954570a9b9ca13a1
Dimitri Sabadie <dimitri.sabadie@ledger.fr>
2019-02-08 10:29:16 +0100

    Update the API’s doc. [skip ci]

--- core/src/api/Currency.hpp
+++ core/src/api/Currency.hpp
@@ -17,6 +17,13 @@
 #include <string>
 #include <utility>
 #include <vector>
+#ifndef LIBCORE_EXPORT
+    #if defined(_MSC_VER) && _MSC_VER <= 1900
+       #include <libcore_export.h>
+    #else
+       #define LIBCORE_EXPORT
+    #endif
+#endif
 
 namespace ledger { namespace core { namespace api {
 
