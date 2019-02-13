05289c78849b611414083299c094d3bf0573accf
Pierre Pollastri <pierre@ledger.fr>
2019-02-13 10:47:32 +0100

    Add StellarLikeWallet class

---
+++ core/src/wallet/stellar/StellarLikeWallet.cpp
@@ -0,0 +1,77 @@
+/*
+ *
+ * StellarLikeWallet.cpp
+ * ledger-core
+ *
+ * Created by Pierre Pollastri on 13/02/2019.
+ *
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2019 Ledger
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
+#include "StellarLikeWallet.hpp"
+
+namespace ledger {
+    namespace core {
+
+        bool StellarLikeWallet::isSynchronizing() {
+            throw make_exception(api::ErrorCode::IMPLEMENTATION_IS_MISSING, "Not implemented");
+        }
+
+        std::shared_ptr<EventBus> StellarLikeWallet::synchronize() {
+            throw make_exception(api::ErrorCode::IMPLEMENTATION_IS_MISSING, "Not implemented");
+        }
+
+        bool StellarLikeWallet::isInstanceOfStellarLikeWallet() {
+            throw make_exception(api::ErrorCode::IMPLEMENTATION_IS_MISSING, "Not implemented");
+        }
+
+        std::shared_ptr<api::StellarLikeWallet> StellarLikeWallet::asStellarLikeWallet() {
+            throw make_exception(api::ErrorCode::IMPLEMENTATION_IS_MISSING, "Not implemented");
+        }
+
+        FuturePtr<api::Account>
+        StellarLikeWallet::newAccountWithInfo(const api::AccountCreationInfo &info) {
+            throw make_exception(api::ErrorCode::IMPLEMENTATION_IS_MISSING, "Not implemented");
+        }
+
+        FuturePtr<ledger::core::api::Account>
+        StellarLikeWallet::newAccountWithExtendedKeyInfo(const api::ExtendedKeyAccountCreationInfo &info) {
+            throw make_exception(api::ErrorCode::IMPLEMENTATION_IS_MISSING, "Not implemented");
+        }
+
+        Future<api::ExtendedKeyAccountCreationInfo>
+        StellarLikeWallet::getExtendedKeyAccountCreationInfo(int32_t accountIndex) {
+            throw make_exception(api::ErrorCode::IMPLEMENTATION_IS_MISSING, "Not implemented");
+        }
+
+        Future<api::AccountCreationInfo> StellarLikeWallet::getAccountCreationInfo(int32_t accountIndex) {
+            throw make_exception(api::ErrorCode::IMPLEMENTATION_IS_MISSING, "Not implemented");
+        }
+
+        std::shared_ptr<AbstractAccount>
+        StellarLikeWallet::createAccountInstance(soci::session &sql, const std::string &accountUid) {
+            throw make_exception(api::ErrorCode::IMPLEMENTATION_IS_MISSING, "Not implemented");
+        }
+    }
+}
---
+++ core/src/wallet/stellar/StellarLikeWallet.hpp
@@ -0,0 +1,72 @@
+/*
+ *
+ * StellarLikeWallet.hpp
+ * ledger-core
+ *
+ * Created by Pierre Pollastri on 13/02/2019.
+ *
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2019 Ledger
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
+#ifndef LEDGER_CORE_STELLARLIKEWALLET_HPP
+#define LEDGER_CORE_STELLARLIKEWALLET_HPP
+
+#include <api/StellarLikeWallet.hpp>
+#include <wallet/common/AbstractWallet.hpp>
+
+namespace ledger {
+    namespace core {
+
+        class StellarLikeWallet : public virtual api::StellarLikeWallet, public virtual AbstractWallet {
+        public:
+            bool isSynchronizing() override;
+
+            std::shared_ptr<api::EventBus> synchronize() override;
+
+            bool isInstanceOfStellarLikeWallet() override;
+
+            std::shared_ptr<api::StellarLikeWallet> asStellarLikeWallet() override;
+
+            FuturePtr<api::Account> newAccountWithInfo(const api::AccountCreationInfo &info) override;
+
+            FuturePtr<api::Account>
+            newAccountWithExtendedKeyInfo(const api::ExtendedKeyAccountCreationInfo &info) override;
+
+            Future<api::ExtendedKeyAccountCreationInfo>
+            getExtendedKeyAccountCreationInfo(int32_t accountIndex) override;
+
+            Future<api::AccountCreationInfo> getAccountCreationInfo(int32_t accountIndex) override;
+
+        protected:
+            std::shared_ptr<AbstractAccount>
+            createAccountInstance(soci::session &sql, const std::string &accountUid) override;
+
+        private:
+            api::StellarLikeNetworkParameters _params;
+        };
+
+    }
+}
+
+#endif //LEDGER_CORE_STELLARLIKEWALLET_HPP
