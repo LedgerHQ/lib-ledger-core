4985376fbadcba6c4c8c4a456ca78c65475b253f
Andrii Korol <andrii.korol@ledger.fr>
2019-02-27 11:35:58 +0100

    Remove build_sqlcipher* scripts

--- core/lib/cmake/install_sqlcipher.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-mkdir -p ../../include/sqlcipher || echo "SQLCipher: include directory exists"
-mkdir -p ../../lib || echo "SQLCipher: lib directory exists"
-cp sqlite3.h ../../include/sqlcipher
-cp sqlite3ext.h ../../include/sqlcipher
-cp .libs/libsqlcipher.a ../../lib
