e03cdd033ea63465d66b26cc89f108a6d8dc1c6a
Dimitri Sabadie <dimitri.sabadie@ledger.fr>
2019-07-03 16:25:57 +0200

    Get rid of old migrations from Core.

--- ledger-core/inc/core/database/migrations.hpp
+++ ledger-core/inc/core/database/migrations.hpp
@@ -75,37 +75,18 @@ namespace ledger {
             return version;
         }
 
-        template <int migrationNumber>
-        void migrate(soci::session& sql) {
-            std::cerr << "No specified migration for version " << migrationNumber << std::endl;
-            throw make_exception(api::ErrorCode::RUNTIME_ERROR, "No specified migration for version {}", migrationNumber);
-        }
-
-        template <int version>
-        bool migrate(soci::session& sql, int currentVersion) {
-            bool previousResult = migrate<version - 1>(sql, currentVersion);
-
-            if (currentVersion < version) {
-                migrate<version>(sql);
-                sql << "UPDATE __database_meta__ SET version = :version", soci::use(version);
-                return true;
-            }
-
-            return previousResult;
-        };
-
         template <int version, typename T>
-        void migrate2(soci::session& sql) {
+        void migrate(soci::session& sql) {
             std::cerr << "No specified migration for version " << version << std::endl;
             throw make_exception(api::ErrorCode::RUNTIME_ERROR, "No specified migration for version {}", version);
         }
 
         template <int version, typename T>
-        bool migrate2(soci::session& sql, int currentVersion) {
-            bool previousResult = migrate2<version - 1, T>(sql, currentVersion);
+        bool migrate(soci::session& sql, int currentVersion) {
+            bool previousResult = migrate<version - 1, T>(sql, currentVersion);
 
             if (currentVersion < version) {
-                migrate2<version, T>(sql);
+                migrate<version, T>(sql);
                 sql << "UPDATE __database_meta__ SET id = :id, version = :version", soci::use(T::coinID), soci::use(version);
 
                 return true;
@@ -114,57 +95,8 @@ namespace ledger {
             return previousResult;
         };
 
-        template <int version>
+        template <int version, typename T>
         void rollback(soci::session& sql) {
-            //std::cerr << "No specified rollback for version " << version << std::endl;
-            //throw make_exception(api::ErrorCode::RUNTIME_ERROR, "No specified rollback for version {}", version);
-        }
-
-        /// Rollback all migrations down.
-        ///
-        /// This is a bit like dropping the content of the database, but does it in a more correct
-        /// and portable way. Also, it enables possible partial rollbacks, even though the current
-        /// implementation doesn’t.
-        template <int version>
-        void rollback(soci::session& sql, int currentVersion) {
-            if (currentVersion == version) {
-                // we’re in sync with the database; perform the rollback normally
-                rollback<version>(sql);
-
-                if (version >= 0) {
-                    // after rolling back this migration, we won’t have anything left, so we only
-                    // update the version for > 0
-                    if (version != 0) {
-                        auto prevVersion = version - 1;
-                        sql << "UPDATE __database_meta__ SET version = :version", soci::use(prevVersion);
-                    }
-
-                    rollback<version - 1>(sql, currentVersion - 1);
-                }
-            } else if (currentVersion < version) {
-                // we’re trying to rollback a migration that hasn’t been applied; try the previous
-                // rollback
-                rollback<version - 1>(sql, currentVersion);
-            } else {
-                // we’re trying to rollback a migration but we have missed some others; apply the
-                // next ones first
-                throw make_exception(api::ErrorCode::RUNTIME_ERROR, "Missing rollback migrations: {} to {}", version + 1, currentVersion);
-            }
-        }
-
-        template <int version, typename T>
-        void rollback2(soci::session& sql) {
-            //std::cerr << "No specified rollback for version " << version << std::endl;
-            //throw make_exception(api::ErrorCode::RUNTIME_ERROR, "No specified rollback for version {}", version);
-        }
-
-        /// Rollback all migrations down.
-        ///
-        /// This is a bit like dropping the content of the database, but does it in a more correct
-        /// and portable way. Also, it enables possible partial rollbacks, even though the current
-        /// implementation doesn’t.
-        template <int version, typename T>
-        void rollback2(soci::session& sql, int currentVersion) {
         }
 
         /// Migration system.
@@ -179,20 +111,20 @@ namespace ledger {
         template <int version, typename T>
         struct Migration final {
           /// Advance the migration system up to migrationNumber.
-          static void migrate(soci::session& sql, int currentVersion) {
-              Migration<version - 1, T>::migrate(sql, currentVersion);
+          static void forward(soci::session& sql, int currentVersion) {
+              Migration<version - 1, T>::forward(sql, currentVersion);
 
               if (currentVersion < version) {
-                  migrate2<version, T>(sql);
+                  migrate<version, T>(sql);
                   sql << "UPDATE __database_meta__ SET id = :id, version = :version", soci::use(T::coinID), soci::use(version);
               }
           }
 
           /// Rollback from migrationNumber.
-          static void rollback(soci::session& sql, int currentVersion) {
+          static void backward(soci::session& sql, int currentVersion) {
               if (currentVersion == version) {
                   // we’re in sync with the database; perform the rollback normally
-                  rollback2<version, T>(sql);
+                  rollback<version, T>(sql);
 
                   if (version >= 0) {
                       // after rolling back this migration, we won’t have anything left, so we only
@@ -202,12 +134,12 @@ namespace ledger {
                           sql << "UPDATE __database_meta__ SET id = :id, version = :version", soci::use(T::coinID), soci::use(prevVersion);
                       }
 
-                      Migration<version - 1, T>::rollback(sql, currentVersion - 1);
+                      Migration<version - 1, T>::backward(sql, currentVersion - 1);
                   }
               } else if (currentVersion < version) {
                   // we’re trying to rollback a migration that hasn’t been applied; try the previous
                   // rollback
-                  Migration<version - 1, T>::rollback(sql, currentVersion);
+                  Migration<version - 1, T>::backward(sql, currentVersion);
               } else {
                   // we’re trying to rollback a migration but we have missed some others; apply the
                   // next ones first
@@ -218,17 +150,14 @@ namespace ledger {
 
         template <typename T>
         struct Migration<0, T> final {
-           static void migrate(soci::session& sql, int currentVersion) {
+           static void forward(soci::session& sql, int currentVersion) {
                sql << "INSERT INTO __database_meta__(id, version) VALUES(:id, 0)", soci::use(T::coinID);
            }
 
-           static void rollback(soci::session& sql, int currentVersion) {
+           static void backward(soci::session& sql, int currentVersion) {
              sql << "DELETE FROM __database_meta__ where id = :id", soci::use(T::coinID);
            }
         };
-
-        template <> bool migrate<-1>(soci::session& sql, int currentVersion);
-        template <> void rollback<-1>(soci::session& sql, int currentVersion);
 
         // default migrations
 
@@ -244,35 +173,7 @@ namespace ledger {
         };
 
         // migrations
-        template <> void migrate2<1, CoreMigration>(soci::session& sql);
-        template <> void rollback2<1, CoreMigration>(soci::session& sql);
-
-        template <> void migrate<0>(soci::session& sql);
-        template <> void rollback<0>(soci::session& sql);
-
-        template <> void migrate<1>(soci::session& sql);
-        template <> void rollback<1>(soci::session& sql);
-
-        template <> void migrate<2>(soci::session& sql);
-        template <> void rollback<2>(soci::session& sql);
-
-        template <> void migrate<3>(soci::session& sql);
-        template <> void rollback<3>(soci::session& sql);
-
-        template <> void migrate<4>(soci::session& sql);
-        template <> void rollback<4>(soci::session& sql);
-
-        template <> void migrate<5>(soci::session& sql);
-        template <> void rollback<5>(soci::session& sql);
-
-        template <> void migrate<6>(soci::session& sql);
-        template <> void rollback<6>(soci::session& sql);
-
-        template <> void migrate<7>(soci::session& sql);
-        template <> void rollback<7>(soci::session& sql);
-
-        // add ripple’s memo field
-        template <> void migrate<8>(soci::session& sql);
-        template <> void rollback<8>(soci::session& sql);
+        template <> void migrate<1, CoreMigration>(soci::session& sql);
+        template <> void rollback<1, CoreMigration>(soci::session& sql);
     }
 }
