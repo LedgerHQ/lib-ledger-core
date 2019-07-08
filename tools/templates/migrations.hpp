1112b4c097d56f2ffb28284fe8f836abad8d0cf5
Dimitri Sabadie <dimitri.sabadie@ledger.fr>
2019-07-08 14:58:52 +0200

    Add tooling for the segregation.

---
+++ tools/templates/migrations.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <core/database/migrations.hpp>
+
+namespace ledger {
+    namespace core {
+        /// Tag type.
+        struct $project_nameMigration {
+          static int const coinID;
+          static uint32_t const currentVersion;
+        };
+
+        // migrations
+        template <> void migrate<1, $project_nameMigration>(soci::session& sql);
+        template <> void rollback<1, $project_nameMigration>(soci::session& sql);
+  }
+}
