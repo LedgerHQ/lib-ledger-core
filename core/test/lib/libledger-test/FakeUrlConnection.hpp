5615b8991923edfca462281cfeb505dd9e7a6104
Andrii Korol <andrii.korol@ledger.fr>
2019-07-19 10:41:15 +0200

    Add FakeHttpClient for testing

---
+++ core/test/lib/libledger-test/FakeUrlConnection.hpp
@@ -0,0 +1,27 @@
+#pragma once
+#include "api/HttpUrlConnection.hpp"
+
+namespace ledger {
+    namespace core {
+        namespace test {
+            struct UrlConnectionData {
+                int32_t statusCode;
+                std::string statusText;
+                std::unordered_map<std::string, std::string> headers;
+                std::string body;
+            };
+
+            class FakeUrlConnection : public api::HttpUrlConnection {
+            public:
+                static std::shared_ptr<FakeUrlConnection> FakeUrlConnection::fromString(const std::string& responseData);
+                FakeUrlConnection(const UrlConnectionData& data);
+                int32_t getStatusCode() override;
+                std::string getStatusText() override;
+                std::unordered_map<std::string, std::string> getHeaders() override;
+                api::HttpReadBodyResult readBody() override;
+            private:
+                UrlConnectionData _data;
+            };
+        }
+    }
+}
