b30f51eb4d0504c9837c04e6f7cc548eff69d0cd
Andrii Korol <andrii.korol@ledger.fr>
2019-09-17 10:43:42 +0200

    Add CryptoUtils
    This is implementation of different utilities
    that doesn't depend on lib-core types for better reusability

--- core/src/math/CryptoUtils.h
+++ core/src/math/CryptoUtils.h
@@ -4,12 +4,19 @@
 namespace ledger {
     namespace core {
         // this is implementation of https://github.com/bitcoin/bips/blob/master/bip-0032.mediawiki#serialization-format
+        template<typename Base58Encoder>
         std::string SerializeExtendedKey(
             const std::vector<uint8_t>& version,
             uint8_t depth,
             const std::vector<uint8_t>& parentFingerPrint,
             uint32_t childNumber,
             const std::vector<uint8_t>& chainCode,
-            const std::vector<uint8_t>& serializedKey);
+            const std::vector<uint8_t>& serializedKey) {
+            std::vector<uint8_t> payload(4 + 1 + 4 + 4 + 32 + 33);
+            std::copy(version.begin(), version.end(), payload.begin());
+            payload[4] = depth;
+            std::copy(parentFingerPrint.begin(), parentFingerPrint.end(), payload.begin() + 5);
+
+        }
     }
 }
