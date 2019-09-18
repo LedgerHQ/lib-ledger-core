3231cbfd70163e02a0a53a0d35851ea02beba6d9
Alexis Le Provost <alexis.le-provost@ledger.fr>
2019-09-18 10:31:04 +0200

    put all enum values in hex

--- core/src/wallet/bitcoin/networks.hpp
+++ core/src/wallet/bitcoin/networks.hpp
@@ -47,9 +47,9 @@ namespace ledger {
         namespace networks {
 
             enum sigHashType : uint8_t {
-                SIGHASH_ALL = 1,
-                SIGHASH_NONE = 2,
-                SIGHASH_SINGLE = 3,
+                SIGHASH_ALL = 0x01,
+                SIGHASH_NONE = 0x02,
+                SIGHASH_SINGLE = 0x03,
                 SIGHASH_FORKID = 0x40,
                 SIGHASH_ANYONECANPAY = 0x80
             };
