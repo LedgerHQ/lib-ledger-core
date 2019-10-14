3e4ce2de0d12cf2983e198857e7ce064f48687a0
Alexis Le Provost <alexis.le-provost@ledger.fr>
2019-10-14 11:05:26 +0200

    added ETH databases

---
+++ ledger-core-ethereum/src/ethereum/database/EthereumLikeAccountDatabaseHelper.cpp
@@ -0,0 +1,101 @@
+/*
+ *
+ * EthereumLikeAccountDatabaseHelper
+ *
+ * Created by El Khalil Bellakrid on 14/07/2018.
+ *
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2018 Ledger
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
+#include <core/wallet/AccountDatabaseHelper.hpp>
+
+#include <ethereum/database/EthereumLikeAccountDatabaseHelper.hpp>
+
+using namespace soci;
+
+namespace ledger {
+    namespace core {
+        void EthereumLikeAccountDatabaseHelper::createAccount(soci::session &sql,
+                                                         const std::string walletUid, int32_t index,
+                                                         const std::string &address) {
+            auto uid = AccountDatabaseHelper::createAccountUid(walletUid, index);
+            sql << "INSERT INTO ethereum_accounts VALUES(:uid, :wallet_uid, :idx, :address)",use(uid), use(walletUid), use(index), use(address);
+        }
+
+        void EthereumLikeAccountDatabaseHelper::createERC20Account(soci::session &sql,
+                                                                   const std::string &ethAccountUid,
+                                                                   const std::string &erc20AccountUid,
+                                                                   const std::string &contractAddress) {
+            sql << "INSERT INTO erc20_accounts VALUES(:uid, :ethereum_account_uid, :contract_address)",use(erc20AccountUid), use(ethAccountUid), use(contractAddress);
+        }
+
+        bool EthereumLikeAccountDatabaseHelper::queryAccount(soci::session &sql,
+                                                             const std::string &accountUid,
+                                                             EthereumLikeAccountDatabaseEntry &entry) {
+            rowset<row> rows = (sql.prepare << "SELECT eth.idx, eth.address, "
+                    "erc20.uid, erc20.contract_address "
+                    "FROM ethereum_accounts AS eth "
+                    "LEFT JOIN erc20_accounts AS erc20 ON erc20.ethereum_account_uid = eth.uid"
+                    " WHERE eth.uid = :uid", use(accountUid));
+            for (auto& row : rows) {
+                if (entry.address.empty()) {
+                    entry.index = row.get<int32_t>(0);
+                    entry.address = row.get<std::string>(1);
+                }
+                if (row.get_indicator(2) != i_null) {
+                    ERC20LikeAccountDatabaseEntry erc20Entry;
+                    erc20Entry.uid = row.get<std::string>(2);
+                    erc20Entry.contractAddress = row.get<std::string>(3);
+                    entry.erc20Accounts.emplace_back(erc20Entry);
+                }
+            }
+            return !entry.address.empty();
+        }
+
+        api::ERC20Token EthereumLikeAccountDatabaseHelper::getOrCreateERC20Token(soci::session &sql,
+                                                                                 const std::string &contractAddress) {
+            api::ERC20Token erc20Token;
+            auto count = 0;
+            sql << "SELECT COUNT(*) FROM erc20_tokens WHERE contract_address = :contract_address", soci::use(contractAddress), soci::into(count);
+            if (count > 0) {
+                soci::rowset<soci::row> rows = (sql.prepare << "SELECT name, symbol, number_of_decimal FROM erc20_tokens WHERE contract_address = :contract_address", soci::use(contractAddress));
+                for (auto& row : rows) {
+                    auto name = row.get<std::string>(0);
+                    auto symbol = row.get<std::string>(1);
+                    auto numberOfDecimals = row.get<int32_t>(2);
+                    erc20Token = api::ERC20Token(name, symbol, contractAddress, numberOfDecimals);
+                }
+            } else {
+                erc20Token = api::ERC20Token("UNKNOWN_TOKEN", "UNKNOWN", contractAddress, 0);
+                sql << "INSERT INTO erc20_tokens VALUES(:contract_address, :name, :symbol, :number_of_decimal)",
+                        use(erc20Token.contractAddress),
+                        use(erc20Token.name),
+                        use(erc20Token.symbol),
+                        use(erc20Token.numberOfDecimal);
+            }
+            return erc20Token;
+        }
+
+    }
+}
---
+++ ledger-core-ethereum/src/ethereum/database/EthereumLikeTransactionDatabaseHelper.cpp
@@ -0,0 +1,162 @@
+/*
+ *
+ * EthereumLikeTransactionDatabaseHelper
+ *
+ * Created by El Khalil Bellakrid on 14/07/2018.
+ *
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2018 Ledger
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
+#include <core/crypto/SHA256.hpp>
+#include <core/database/SociDate.hpp>
+#include <core/database/SociNumber.hpp>
+#include <core/database/SociOption.hpp>
+#include <core/math/BigInt.hpp>
+#include <core/utils/Hex.hpp>
+#include <core/wallet/BlockDatabaseHelper.hpp>
+
+#include <ethereum/database/EthereumLikeTransactionDatabaseHelper.hpp>
+
+using namespace soci;
+
+namespace ledger {
+    namespace core {
+
+        bool EthereumLikeTransactionDatabaseHelper::getTransactionByHash(soci::session &sql,
+                                                                         const std::string &hash,
+                                                                         EthereumLikeBlockchainExplorerTransaction &tx) {
+
+            rowset<row> rows = (sql.prepare << "SELECT  tx.hash, tx.value, tx.nonce, tx.time, tx.input_data, tx.gas_price, "
+                                                "tx.gas_limit, tx.gas_used, tx.sender, tx.receiver, tx.confirmations, tx.status, "
+                                                "block.hash, block.height, block.time, block.currency_name "
+                                                "FROM ethereum_transactions AS tx "
+                                                "LEFT JOIN blocks AS block ON tx.block_uid = block.uid "
+                                                "WHERE tx.hash = :hash", use(hash));
+
+            for (auto& row : rows) {
+                inflateTransaction(sql, row, tx);
+                return true;
+            }
+
+            return false;
+        }
+
+        bool EthereumLikeTransactionDatabaseHelper::inflateTransaction(soci::session &sql,
+                                                                       const soci::row &row,
+                                                                       EthereumLikeBlockchainExplorerTransaction &tx) {
+            tx.hash = row.get<std::string>(0);
+            tx.value = BigInt::fromHex(row.get<std::string>(1));
+            auto nonceBytes = hex::toByteArray(row.get<std::string>(2));
+            auto shift = 0;
+            for (auto& byte : nonceBytes) {
+                tx.nonce += byte << shift;
+                shift += 8;
+            }
+
+            tx.receivedAt = row.get<std::chrono::system_clock::time_point>(3);
+            tx.inputData = hex::toByteArray(row.get<std::string>(4));
+
+            tx.gasPrice = BigInt::fromHex(row.get<std::string>(5));
+            tx.gasLimit = BigInt::fromHex(row.get<std::string>(6));
+            tx.gasUsed = BigInt::fromHex(row.get<std::string>(7));
+
+            tx.sender = row.get<std::string>(8);
+            tx.receiver = row.get<std::string>(9);
+
+            tx.confirmations = get_number<uint64_t>(row, 10);
+            tx.status = get_number<uint64_t>(row, 11);
+            if (row.get_indicator(12) != i_null) {
+                EthereumLikeBlockchainExplorer::Block block;
+                block.blockHash = row.get<std::string>(12);
+                block.height = get_number<uint64_t>(row, 13);
+                block.time = row.get<std::chrono::system_clock::time_point>(14);
+                block.currencyName = row.get<std::string>(15);
+                tx.block = block;
+            }
+
+            return true;
+        }
+
+        bool EthereumLikeTransactionDatabaseHelper::transactionExists(soci::session &sql,
+                                                                      const std::string &ethTxUid) {
+            int32_t count = 0;
+            sql << "SELECT COUNT(*) FROM ethereum_transactions WHERE transaction_uid = :ethTxUid", use(ethTxUid), into(count);
+            return count == 1;
+        }
+
+        std::string EthereumLikeTransactionDatabaseHelper::createEthereumTransactionUid(const std::string& accountUid,
+                                                                                        const std::string& txHash) {
+            auto result = SHA256::stringToHexHash(fmt::format("uid:{}+{}", accountUid, txHash));
+            return result;
+        }
+
+        std::string EthereumLikeTransactionDatabaseHelper::putTransaction(soci::session &sql,
+                                                                         const std::string& accountUid,
+                                                                         const EthereumLikeBlockchainExplorerTransaction &tx) {
+            auto blockUid = tx.block.map<std::string>([] (const EthereumLikeBlockchainExplorer::Block& block) {
+                return block.uid;
+            });
+
+            auto ethTxUid = createEthereumTransactionUid(accountUid, tx.hash);
+
+            if (transactionExists(sql, ethTxUid)) {
+                // UPDATE
+                if (tx.block.nonEmpty()) {
+                    auto gasUsed = tx.gasUsed.getValueOr(BigInt::ZERO).toHexString();
+                    sql << "UPDATE ethereum_transactions SET block_uid = :uid, status = :code, gas_used = :gas WHERE hash = :tx_hash",
+                            use(blockUid), use(tx.status), use(gasUsed), use(tx.hash);
+                }
+                return ethTxUid;
+            } else {
+                // Insert
+                if (tx.block.nonEmpty()) {
+                    BlockDatabaseHelper::putBlock(sql, tx.block.getValue());
+                }
+
+                auto txInputData = hex::toString(tx.inputData);
+                auto hexTxValue = tx.value.toHexString();
+                auto hexGasPrice = tx.gasPrice.toHexString();
+                auto hexGasLimit = tx.gasLimit.toHexString();
+                auto hexGasUsed = tx.gasUsed.getValueOr(BigInt::ZERO).toHexString();
+                sql << "INSERT INTO ethereum_transactions VALUES(:tx_uid, :hash, :nonce, :value, :block_uid, :time, :sender, :receiver, :input_data, :gasPrice, :gasLimit, :gasUsed, :confirmations, :status)",
+                        use(ethTxUid),
+                        use(tx.hash),
+                        use(tx.nonce),
+                        use(hexTxValue),
+                        use(blockUid),
+                        use(tx.receivedAt),
+                        use(tx.sender),
+                        use(tx.receiver),
+                        use(txInputData),
+                        use(hexGasPrice),
+                        use(hexGasLimit),
+                        use(hexGasUsed),
+                        use(tx.confirmations),
+                        use(tx.status);
+
+                return ethTxUid;
+            }
+        }
+    }
+}
