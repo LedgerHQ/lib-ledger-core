b46ac1371f759cca23b07c576e70a38f0ab2eefe
Alexis Le Provost <alexis.le-provost@ledger.fr>
2019-11-19 15:43:28 +0100

    [tezos] migrate factories

---
+++ ledger-core-tezos/inc/tezos/factories/TezosLikeWalletFactory.hpp
@@ -0,0 +1,69 @@
+/*
+ *
+ * TezosLikeWalletFactory
+ *
+ * Created by El Khalil Bellakrid on 27/04/2019.
+ *
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2019 Ledger
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
+#pragma once
+
+#include <functional>
+
+#include <tezos/explorers/TezosLikeBlockchainExplorer.hpp>
+#include <tezos/factories/TezosLikeKeychainFactory.hpp>
+#include <tezos/observers/TezosLikeBlockchainObserver.hpp>
+#include <tezos/synchronizers/TezosLikeAccountSynchronizer.hpp>
+
+#include <core/api/Currency.hpp>
+#include <core/wallet/AbstractWalletFactory.hpp>
+#include <core/Services.hpp>
+
+namespace ledger {
+    namespace core {
+        using TezosLikeAccountSynchronizerFactory = std::function<std::shared_ptr<TezosLikeAccountSynchronizer>()>;
+
+        class WalletPool;
+
+        class TezosLikeWalletFactory : public AbstractWalletFactory {
+        public:
+            TezosLikeWalletFactory(const api::Currency &currency, const std::shared_ptr<Services> &services);
+
+            std::shared_ptr<AbstractWallet> build(const WalletDatabaseEntry &entry) override;
+
+        private:
+            std::shared_ptr<TezosLikeBlockchainExplorer>
+            getExplorer(const std::string &currencyName, const std::shared_ptr<api::DynamicObject> &configuration);
+
+            std::shared_ptr<TezosLikeBlockchainObserver>
+            getObserver(const std::string &currencyName, const std::shared_ptr<api::DynamicObject> &configuration);
+
+        private:
+            std::list<std::weak_ptr<TezosLikeBlockchainExplorer>> _runningExplorers;
+            std::list<std::weak_ptr<TezosLikeBlockchainObserver>> _runningObservers;
+            std::unordered_map<std::string, std::shared_ptr<TezosLikeKeychainFactory>> _keychainFactories;
+        };
+    }
+}
