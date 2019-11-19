02c55fb662d5594c1d8f2b453f3f2a07ff09ec7f
Alexis Le Provost <alexis.le-provost@ledger.fr>
2019-11-19 15:51:09 +0100

    [tezos] migrate transactions

---
+++ ledger-core-tezos/inc/tezos/transactions/TezosLikeTransaction.hpp
@@ -0,0 +1,130 @@
+/*
+ *
+ * TezosLikeTransaction
+ *
+ * Created by El Khalil Bellakrid on 27/04/2019.
+ *
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2019 Ledger
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
+#pragma once
+
+#include <tezos/TezosLikeBlock.hpp>
+#include <tezos/api/TezosLikeTransaction.hpp>
+#include <tezos/api/TezosOperationTag.hpp>
+#include <tezos/api/TezosCurve.hpp>
+#include <tezos/operations/TezosLikeOperation.hpp>
+#include <tezos/explorers/TezosLikeBlockchainExplorer.hpp>
+
+#include <core/api/Amount.hpp>
+#include <core/api/Currency.hpp>
+#include <core/math/BigInt.hpp>
+#include <core/operation/Operation.hpp>
+#include <core/operation/OperationQuery.hpp>
+
+namespace ledger {
+    namespace core {
+        // Reference: https://github.com/obsidiansystems/ledger-app-tezos/blob/9a0c8cc546677147b93935e0b0c96925244baf64/src/types.h
+        class TezosLikeTransaction : public api::TezosLikeTransaction {
+        public:
+            explicit TezosLikeTransaction(const api::Currency &currency);
+
+            explicit TezosLikeTransaction(
+                const TezosLikeBlockchainExplorerTransaction& tx,
+                const api::Currency &currency);
+
+            api::TezosOperationTag getType() override;
+
+            std::string getHash() override;
+
+            std::shared_ptr<api::Amount> getFees() override;
+
+            std::shared_ptr<api::TezosLikeAddress> getReceiver() override;
+
+            std::shared_ptr<api::TezosLikeAddress> getSender() override;
+
+            std::shared_ptr<api::Amount> getValue() override;
+
+            std::vector<uint8_t> serialize() override;
+
+            std::chrono::system_clock::time_point getDate() override;
+
+            std::shared_ptr<api::BigInt> getCounter() override;
+
+            std::shared_ptr<api::Amount> getGasLimit() override ;
+
+            std::shared_ptr<api::BigInt> getStorageLimit() override;
+
+            std::experimental::optional<std::string> getBlockHash() override;
+
+            void setSignature(const std::vector<uint8_t> &signature) override;
+
+            std::vector<uint8_t> getSigningPubKey() override;
+
+            TezosLikeTransaction &setFees(const std::shared_ptr<BigInt> &fees);
+
+            TezosLikeTransaction &setValue(const std::shared_ptr<BigInt> &value);
+
+            TezosLikeTransaction &setSender(const std::shared_ptr<api::TezosLikeAddress> &sender, api::TezosCurve curve = api::TezosCurve::ED25519);
+
+            TezosLikeTransaction &setReceiver(const std::shared_ptr<api::TezosLikeAddress> &receiver, api::TezosCurve curve = api::TezosCurve::ED25519);
+
+            TezosLikeTransaction &setSigningPubKey(const std::vector<uint8_t> &pubKey);
+
+            TezosLikeTransaction &setHash(const std::string &hash);
+
+            TezosLikeTransaction &setBlockHash(const std::string &blockHash);
+
+            TezosLikeTransaction & setGasLimit(const std::shared_ptr<BigInt>& gasLimit);
+            
+            TezosLikeTransaction & setCounter(const std::shared_ptr<BigInt>& counter);
+
+            TezosLikeTransaction & setStorage(const std::shared_ptr<BigInt>& storage);
+
+            TezosLikeTransaction & setType(api::TezosOperationTag type);
+
+            TezosLikeTransaction & setBalance(const BigInt &balance);
+
+        private:
+            std::chrono::system_clock::time_point _time;
+            std::shared_ptr<TezosLikeBlock> _block;
+            std::string _hash;
+            api::Currency _currency;
+            std::shared_ptr<api::Amount> _fees;
+            std::shared_ptr<api::Amount> _gasLimit;
+            std::shared_ptr<BigInt> _storage;
+            std::shared_ptr<BigInt> _counter;
+            std::shared_ptr<api::Amount> _value;
+            std::shared_ptr<api::TezosLikeAddress> _receiver;
+            api::TezosCurve _receiverCurve;
+            std::shared_ptr<api::TezosLikeAddress> _sender;
+            api::TezosCurve _senderCurve;
+            std::vector<uint8_t> _signature;
+            std::vector<uint8_t> _signingPubKey;
+            api::TezosOperationTag _type;
+            std::string _revealedPubKey;
+            BigInt _balance;
+        };
+    }
+}
---
+++ ledger-core-tezos/src/tezos/transactions/TezosLikeTransactionBuilder.cpp
@@ -0,0 +1,296 @@
+/*
+ *
+ * TezosLikeTransactionBuilder
+ *
+ * Created by El Khalil Bellakrid on 27/04/2019.
+ *
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2019 Ledger
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
+
+#include <tezos/TezosNetworks.hpp>
+#include <tezos/api/TezosCurve.hpp>
+#include <tezos/api/TezosOperationTag.hpp>
+#include <tezos/transactions/TezosLikeTransaction.hpp>
+#include <tezos/transactions/TezosLikeTransactionBuilder.hpp>
+
+#include <core/bytes/Zarith.hpp>
+#include <core/math/Base58.hpp>
+
+namespace ledger {
+    namespace core {
+
+        TezosLikeTransactionBuilder::TezosLikeTransactionBuilder(
+                const std::string &senderAddress,
+                const std::shared_ptr<api::ExecutionContext> &context,
+                const api::Currency &currency,
+                const std::shared_ptr<TezosLikeBlockchainExplorer> &explorer,
+                const std::shared_ptr<spdlog::logger> &logger,
+                const TezosLikeTransactionBuildFunction &buildFunction) :
+                _senderAddress(senderAddress), _context(context),
+                _currency(currency), _explorer(explorer),
+                _build(buildFunction), _logger(logger){
+            _request.wipe = false;
+        }
+
+        TezosLikeTransactionBuilder::TezosLikeTransactionBuilder(const TezosLikeTransactionBuilder &cpy) {
+            _currency = cpy._currency;
+            _build = cpy._build;
+            _request = cpy._request;
+            _context = cpy._context;
+            _logger = cpy._logger;
+        }
+
+        std::shared_ptr<api::TezosLikeTransactionBuilder>
+        TezosLikeTransactionBuilder::setType(api::TezosOperationTag type) {
+            _request.type = type;
+            return shared_from_this();
+        }
+
+        std::shared_ptr<api::TezosLikeTransactionBuilder>
+        TezosLikeTransactionBuilder::sendToAddress(const std::shared_ptr<api::Amount> &amount,
+                                                   const std::string &address) {
+            // XTZ self-transactions seems to always fail and existing wallet forbid self-transactions
+            // So that's why we prevent user from creating a self-transaction
+            // TODO: Get reference to confirm this
+            if (address == _senderAddress) {
+                throw make_exception(api::ErrorCode::INVALID_ARGUMENT, "Can not send funds to sending address !");
+            }
+            _request.value = std::make_shared<BigInt>(amount->toString());
+            _request.toAddress = address;
+            return shared_from_this();
+        }
+
+        std::shared_ptr<api::TezosLikeTransactionBuilder>
+        TezosLikeTransactionBuilder::wipeToAddress(const std::string &address) {
+            _request.toAddress = address;
+            _request.wipe = true;
+            return shared_from_this();
+        }
+
+        std::shared_ptr<api::TezosLikeTransactionBuilder>
+        TezosLikeTransactionBuilder::setFees(const std::shared_ptr<api::Amount> &fees) {
+            _request.fees = std::make_shared<BigInt>(fees->toString());
+            return shared_from_this();
+        }
+
+        std::shared_ptr<api::TezosLikeTransactionBuilder>
+        TezosLikeTransactionBuilder::setGasLimit(const std::shared_ptr<api::Amount> & gasLimit) {
+            _request.gasLimit = std::make_shared<BigInt>(gasLimit->toString());
+            return shared_from_this();
+        }
+
+        std::shared_ptr<api::TezosLikeTransactionBuilder>
+        TezosLikeTransactionBuilder::setStorageLimit(const std::shared_ptr<api::BigInt> & storageLimit) {
+            _request.storageLimit = std::make_shared<BigInt>(storageLimit->toString(10));
+            return shared_from_this();
+        }
+
+        void TezosLikeTransactionBuilder::build(
+            const std::function<void(std::shared_ptr<api::TezosLikeTransaction>, std::experimental::optional<api::Error>)> &callback) {
+            build().callback(_context, callback);
+        }
+
+        Future<std::shared_ptr<api::TezosLikeTransaction>> TezosLikeTransactionBuilder::build() {
+            return _build(_request, _explorer);
+        }
+
+        std::shared_ptr<api::TezosLikeTransactionBuilder> TezosLikeTransactionBuilder::clone() {
+            return std::make_shared<TezosLikeTransactionBuilder>(*this);
+        }
+
+        void TezosLikeTransactionBuilder::reset() {
+            _request = TezosLikeTransactionBuildRequest();
+        }
+
+        std::shared_ptr<api::TezosLikeTransaction>
+        api::TezosLikeTransactionBuilder::parseRawUnsignedTransaction(const api::Currency &currency,
+                                                                      const std::vector<uint8_t> &rawTransaction) {
+            return ::ledger::core::TezosLikeTransactionBuilder::parseRawTransaction(currency, rawTransaction, false);
+        }
+
+        std::shared_ptr<api::TezosLikeTransaction>
+        api::TezosLikeTransactionBuilder::parseRawSignedTransaction(const api::Currency &currency,
+                                                                    const std::vector<uint8_t> &rawTransaction) {
+            return ::ledger::core::TezosLikeTransactionBuilder::parseRawTransaction(currency, rawTransaction, true);
+        }
+
+        // Reference: https://www.ocamlpro.com/2018/11/21/an-introduction-to-tezos-rpcs-signing-operations/
+        std::shared_ptr<api::TezosLikeTransaction>
+        TezosLikeTransactionBuilder::parseRawTransaction(const api::Currency &currency,
+                                                         const std::vector<uint8_t> &rawTransaction,
+                                                         bool isSigned) {
+            auto params = networks::getTezosLikeNetworkParameters(currency.name);
+            auto tx = std::make_shared<TezosLikeTransaction>(currency);
+            BytesReader reader(rawTransaction);
+            if (!isSigned) {
+                // Watermark: Generic-Operation
+                reader.readNextByte();
+            }
+
+            // Block Hash
+            auto blockHashBytes = reader.read(32);
+            auto config = std::make_shared<DynamicObject>();
+            config->putString("networkIdentifier", params.Identifier);
+            // Magic bytes (or version ?)
+            std::vector<uint8_t> blockPrefix {0x01, 0x34};
+            auto blockHash = Base58::encodeWithChecksum(vector::concat(blockPrefix, blockHashBytes), config);
+            tx->setBlockHash(blockHash);
+
+            // Operation Tag
+            auto OpTag = reader.readNextByte();
+            tx->setType(static_cast<api::TezosOperationTag>(OpTag));
+
+            // Get Sender
+            // Originated
+            auto isSenderOriginated = reader.readNextByte();
+            // sender hash160
+            std::vector<uint8_t> senderHash160;
+            if (isSenderOriginated) {
+                // Then we read 20 bytes of publicKey hash
+                senderHash160 = reader.read(20);
+                // ... and padding
+                reader.readNextByte();
+            } else {
+                // Otherwise first curve code ...
+                auto senderCurveCode = reader.readNextByte();
+                // then hash160 ...
+                senderHash160 = reader.read(20);
+            }
+            tx->setSender(std::make_shared<TezosLikeAddress>(currency,
+                                                             senderHash160,
+                                                             isSenderOriginated ? params.OriginatedPrefix : params.ImplicitPrefix,
+                                                             Option<std::string>()));
+
+            // Fee
+            tx->setFees(std::make_shared<BigInt>(BigInt::fromHex(hex::toString(zarith::zParse(reader)))));
+
+            // Counter
+            tx->setCounter(std::make_shared<BigInt>(BigInt::fromHex(hex::toString(zarith::zParse(reader)))));
+
+            // Gas Limit
+            tx->setGasLimit(std::make_shared<BigInt>(BigInt::fromHex(hex::toString(zarith::zParse(reader)))));
+
+            // Storage Limit
+            tx->setStorage(std::make_shared<BigInt>(BigInt::fromHex(hex::toString(zarith::zParse(reader)))));
+
+            switch (OpTag) {
+                case static_cast<uint8_t>(api::TezosOperationTag::OPERATION_TAG_REVEAL): {
+                    auto curveCode = reader.readNextByte();
+                    std::vector<uint8_t> pubKey;
+                    switch (curveCode) {
+                        case static_cast<uint8_t>(api::TezosCurve::ED25519):
+                            pubKey = reader.read(32);
+                            break;
+                        default:
+                            pubKey = reader.read(33);
+                            break;
+                    }
+                    tx->setSigningPubKey(pubKey);
+                    tx->setValue(std::make_shared<BigInt>(BigInt::ZERO));
+                    tx->setReceiver(std::make_shared<TezosLikeAddress>(currency,
+                                                                       std::vector<uint8_t>(),
+                                                                       std::vector<uint8_t>(),
+                                                                       Option<std::string>()));
+                    break;
+                }
+                case static_cast<uint8_t>(api::TezosOperationTag::OPERATION_TAG_TRANSACTION): {
+                    // Amount
+                    tx->setValue(std::make_shared<BigInt>(BigInt::fromHex(hex::toString(zarith::zParse(reader)))));
+
+                    // Get Receiver
+                    // Originated
+                    auto isReceiverOriginated = reader.readNextByte();
+                    std::vector<uint8_t> receiverHash160;
+                    if (!isReceiverOriginated) {
+                        // Curve code
+                        auto receiverCurveCode = reader.readNextByte();
+                        // 20 bytes of publicKey hash
+                        receiverHash160 = reader.read(20);
+                    } else {
+                        // 20 bytes of publicKey hash
+                        receiverHash160 = reader.read(20);
+                        // Padding
+                        reader.readNextByte();
+                    }
+
+                    tx->setReceiver(std::make_shared<TezosLikeAddress>(currency,
+                                                                       receiverHash160,
+                                                                       isReceiverOriginated ? params.OriginatedPrefix : params.ImplicitPrefix,
+                                                                       Option<std::string>()));
+
+                    // Additional parameters
+                    auto haveAdditionalParams = reader.readNextByte();
+                    if (haveAdditionalParams) {
+                        auto sizeAdditionals = reader.readNextBeUint();
+                        auto additionals = reader.read(sizeAdditionals);
+                    }
+                    break;
+                }
+                case static_cast<uint8_t>(api::TezosOperationTag::OPERATION_TAG_ORIGINATION): {
+                    tx->setValue(std::make_shared<BigInt>(BigInt::ZERO));
+                    tx->setReceiver(std::make_shared<TezosLikeAddress>(currency,
+                                                                       std::vector<uint8_t>(),
+                                                                       std::vector<uint8_t>(),
+                                                                       Option<std::string>()));
+                    auto managerCurveCode = reader.readNextByte();
+                    // manager hash160
+                    auto managerHash160 = reader.read(20);
+                    if (managerHash160 != senderHash160) {
+                        throw make_exception(api::ErrorCode::INVALID_ARGUMENT, "Origination error: Manager is diffrent from sender");
+                    }
+                    // Balance
+                    tx->setBalance(BigInt::fromHex(hex::toString(zarith::zParse(reader))));
+                    // Is spendable? Is delegatable? Presence of delegate block? Presence of script block?
+                    reader.read(4);
+                    break;
+                }
+                case static_cast<uint8_t>(api::TezosOperationTag::OPERATION_TAG_DELEGATION): {
+                    tx->setValue(std::make_shared<BigInt>(BigInt::ZERO));
+                    auto hasDelegationData = reader.readNextByte();
+                    if (hasDelegationData) {
+                        // Curve Code
+                        auto delegateCurveCode = reader.readNextByte();
+                        // Delegate hash160
+                        auto delegateHash160 = reader.read(20);
+                        tx->setReceiver(std::make_shared<TezosLikeAddress>(currency,
+                                                                           delegateHash160,
+                                                                           params.ImplicitPrefix, // can't delegate to originated account (TBC)
+                                                                           Option<std::string>()));
+                    }
+                    break;
+                }
+                default:
+                    break;
+            }
+
+            // Parse signature
+            if (isSigned) {
+                tx->setSignature(reader.read(64));
+            }
+
+            return tx;
+        }
+    }
+}
