8f712eb796c146f65f7641be081c0a10c0ed5a93
Alexis Le Provost <alexis.le-provost@ledger.fr>
2020-01-21 15:08:05 +0100

    [bitcoin] segregate block

---
+++ ledger-core-bitcoin/src/bitcoin/block/BitcoinLikeBlock.cpp
@@ -0,0 +1,48 @@
+/*
+ *
+ * BitcoinLikeBlock
+ * ledger-core
+ *
+ * Created by Pierre Pollastri on 12/07/2017.
+ *
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2016 Ledger
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
+#include <bitcoin/block/BitcoinLikeBlock.hpp>
+
+std::string ledger::core::BitcoinLikeBlock::getHash() {
+    return _block.blockHash;
+}
+
+int64_t ledger::core::BitcoinLikeBlock::getHeight() {
+    return _block.height;
+}
+
+std::chrono::system_clock::time_point ledger::core::BitcoinLikeBlock::getTime() {
+    return _block.time;
+}
+
+ledger::core::BitcoinLikeBlock::BitcoinLikeBlock(const api::Block &block) {
+    _block = block;
+}
