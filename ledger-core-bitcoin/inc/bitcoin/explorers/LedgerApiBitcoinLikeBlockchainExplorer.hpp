743e0384aef4e5ca29762e86faf62bf60d03b625
Alexis Le Provost <alexis.le-provost@ledger.fr>
2020-01-21 15:09:48 +0100

    [bitcoin] segregate explorers

---
+++ ledger-core-bitcoin/inc/bitcoin/explorers/LedgerApiBitcoinLikeBlockchainExplorer.hpp
@@ -0,0 +1,95 @@
+/*
+ *
+ * LedgerApiBitcoinLikeBlockchainExplorer
+ * ledger-core
+ *
+ * Created by Pierre Pollastri on 10/03/2017.
+ *
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2016 Ledger
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
+#pragma once
+
+#include <core/async/DedicatedContext.hpp>
+#include <core/async/Future.hpp>
+#include <core/collections/Collections.hpp>
+#include <core/explorers/AbstractLedgerApiBlockchainExplorer.hpp>
+#include <core/net/HttpClient.hpp>
+
+#include <bitcoin/api/BitcoinLikeNetworkParameters.hpp>
+#include <bitcoin/block/BitcoinLikeBlockParser.hpp>
+#include <bitcoin/explorers/BitcoinLikeBlockchainExplorer.hpp>
+#include <bitcoin/transactions/BitcoinLikeTransactionParser.hpp>
+#include <bitcoin/transactions/BitcoinLikeTransactionsBulkParser.hpp>
+
+namespace ledger {
+    namespace core {
+
+        using LedgerApiBlockchainExplorer = AbstractLedgerApiBlockchainExplorer<
+            BitcoinLikeBlockchainExplorerTransaction,
+            BitcoinLikeBlockchainExplorer::TransactionsBulk,
+            BitcoinLikeAbstractTransactionParser,
+            BitcoinLikeAbstractTransactionBulkParser,
+            BitcoinLikeBlockParser,
+            api::BitcoinLikeNetworkParameters>;
+
+        class LedgerApiBitcoinLikeBlockchainExplorer : public BitcoinLikeBlockchainExplorer,
+                                                       public LedgerApiBlockchainExplorer,
+                                                       public DedicatedContext,
+                                                       public std::enable_shared_from_this<LedgerApiBitcoinLikeBlockchainExplorer> {
+        public:
+            LedgerApiBitcoinLikeBlockchainExplorer(
+                const std::shared_ptr<api::ExecutionContext>& context,
+                const std::shared_ptr<HttpClient>& http,
+                const api::BitcoinLikeNetworkParameters& parameters,
+                const std::shared_ptr<api::DynamicObject>& configuration
+            );
+
+            Future<String> pushLedgerApiTransaction(const std::vector<uint8_t> &transaction) override;
+            Future<void *> startSession() override;
+            Future<Unit> killSession(void *session) override;
+            Future<Bytes> getRawTransaction(const String& transactionHash) override;
+            Future<String> pushTransaction(const std::vector<uint8_t>& transaction) override;
+
+            FuturePtr<TransactionsBulk>
+            getTransactions(const std::vector<std::string> &addresses,
+                            Option<std::string> fromBlockHash = Option<std::string>(),
+                            Option<void *> session = Option<void *>()) override;
+
+            FuturePtr<Block> getCurrentBlock() const override;
+
+            FuturePtr<BitcoinLikeBlockchainExplorerTransaction> getTransactionByHash(const String &transactionHash) const override;
+
+            Future<int64_t > getTimestamp() const override;
+
+            std::shared_ptr<api::ExecutionContext> getExplorerContext() const override;
+            api::BitcoinLikeNetworkParameters getNetworkParameters() const override;
+            std::string getExplorerVersion() const override;
+            Future<std::vector<std::shared_ptr<api::BigInt>>> getFees() override;
+        private:
+            api::BitcoinLikeNetworkParameters _parameters;
+            std::string _explorerVersion;
+        };
+    }
+}
---
+++ ledger-core-bitcoin/inc/bitcoin/explorers/WebSocketNotificationParser.hpp
@@ -0,0 +1,90 @@
+/*
+ *
+ * WebSocketNotificationParser.h
+ * ledger-core
+ *
+ * Created by Pierre Pollastri on 10/10/2017.
+ *
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2017 Ledger
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
+#pragma once
+
+#include <cstdio>
+#include <cstdint>
+#include <stack>
+
+#include <rapidjson/reader.h>
+
+#include <core/collections/Collections.hpp>
+#include <core/explorers/AbstractWebSocketNotificationParser.hpp>
+#include <core/net/HttpClient.hpp>
+
+#include <bitcoin/block/BitcoinLikeBlockParser.hpp>
+#include <bitcoin/explorers/BitcoinLikeBlockchainExplorer.hpp>
+#include <bitcoin/transactions/BitcoinLikeTransactionParser.hpp>
+
+namespace ledger {
+    namespace core {
+        class WebSocketNotificationParser : public AbstractWebSocketNotificationParser<
+            BitcoinLikeBlockchainExplorerTransaction,
+            BitcoinLikeBlockchainExplorer::Block, 
+            BitcoinLikeTransactionParser, 
+            BitcoinLikeBlockParser> {
+        public:
+
+
+            explicit WebSocketNotificationParser(std::string& lastKey) : _lastKey(lastKey),
+                                                                        _blockParser(lastKey),
+                                                                        _transactionParser(lastKey) {
+
+            }
+
+            bool Key(const rapidjson::Reader::Ch* str, rapidjson::SizeType length, bool copy) override {
+                _lastKey = std::string(str, length);
+                return AbstractWebSocketNotificationParser<BitcoinLikeBlockchainExplorerTransaction,
+                        BitcoinLikeBlockchainExplorer::Block,
+                        BitcoinLikeTransactionParser,
+                        BitcoinLikeBlockParser>::Key(str, length, copy);
+            }
+
+        protected:
+
+            BitcoinLikeTransactionParser &getTransactionParser() override {
+              return _transactionParser;
+            };
+            BitcoinLikeBlockParser &getBlockParser() override {
+                return _blockParser;
+            };
+            std::string &getLastKey() override {
+                return _lastKey;
+            };
+
+        private:
+            std::string& _lastKey;
+            BitcoinLikeBlockParser _blockParser;
+            BitcoinLikeTransactionParser _transactionParser;
+        };
+    }
+}
