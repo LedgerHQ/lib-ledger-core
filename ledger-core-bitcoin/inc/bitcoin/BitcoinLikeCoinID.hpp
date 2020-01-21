28e97d877a7adcacae4278cd48181021371d22e4
Alexis Le Provost <alexis.le-provost@ledger.fr>
2020-01-21 15:30:56 +0100

    [bitcoin] segregate currencies

---
+++ ledger-core-bitcoin/inc/bitcoin/BitcoinLikeCoinID.hpp
@@ -0,0 +1,61 @@
+/*
+ *
+ * RippleLikeCoinID
+ *
+ * Created by Alexis Le Provost on 2019/12/12.
+ *
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2019 Ledger
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
+#pragma once
+
+#include <stdint.h>
+
+namespace ledger {
+    namespace core {
+        static uint64_t constexpr BITCOIN_COIN_ID = 0;
+        static uint64_t constexpr BITCOIN_TESTNET_COIN_ID = 1;
+        static uint64_t constexpr BITCOIN_CASH_COIN_ID = 145;
+        static uint64_t constexpr BITCOIN_GOLD_COIN_ID = 156;
+        static uint64_t constexpr ZCASH_COIN_ID = 121;
+        static uint64_t constexpr LITE_COIN_ID = 2;
+        static uint64_t constexpr PEERCOIN_COIN_ID = 6;
+        static uint64_t constexpr DIGIBYTE_COIN_ID = 20;
+        static uint64_t constexpr HCASH_COIN_ID = 171;
+        static uint64_t constexpr QTUM_COIN_ID = 88;
+        static uint64_t constexpr STEALTHCOIN_COIN_ID = 125;
+        static uint64_t constexpr VERTCOIN_COIN_ID = 128;
+        static uint64_t constexpr VIACOIN_COIN_ID = 14;
+        static uint64_t constexpr DASH_COIN_ID = 5;
+        static uint64_t constexpr DOGECOIN_COIN_ID = 3;
+        static uint64_t constexpr STRATIS_COIN_ID = 105;
+        static uint64_t constexpr KOMODO_COIN_ID = 141;
+        static uint64_t constexpr POSWALLET_COIN_ID = 47;
+        static uint64_t constexpr PIVX_COIN_ID = 77;
+        static uint64_t constexpr CLUBCOIN_COIN_ID = 79;
+        static uint64_t constexpr DECRED_COIN_ID = 42;
+        static uint64_t constexpr STAKENET_COIN_ID = 384;
+        
+    }
+}
