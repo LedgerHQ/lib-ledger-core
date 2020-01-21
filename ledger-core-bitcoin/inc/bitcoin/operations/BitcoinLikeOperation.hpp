725e1fff138094d57944aa2226b5163c50611d13
Alexis Le Provost <alexis.le-provost@ledger.fr>
2020-01-21 15:20:32 +0100

    [bitcoin] segregate operations

---
+++ ledger-core-bitcoin/inc/bitcoin/operations/BitcoinLikeOperation.hpp
@@ -0,0 +1,70 @@
+/*
+ *
+ * BitcoinLikeOperation
+ * ledger-core
+ *
+ * Created by Pierre Pollastri on 12/07/2017.
+ *
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2016 Ledger
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
+#pragma once
+
+#include <memory>
+
+#include <core/operation/Operation.hpp>
+#include <core/wallet/AbstractWallet.hpp>
+
+#include <bitcoin/api/BitcoinLikeOperation.hpp>
+#include <bitcoin/api/BitcoinLikeTransaction.hpp>
+#include <bitcoin/explorers/BitcoinLikeBlockchainExplorer.hpp>
+
+namespace ledger {
+    namespace core {
+        class BitcoinLikeOperation : public api::BitcoinLikeOperation, public Operation {
+        public:
+            BitcoinLikeOperation() = default;
+
+            BitcoinLikeOperation(
+                const std::shared_ptr<const AbstractWallet>& wallet,
+                BitcoinLikeBlockchainExplorerTransaction const& tx);
+
+            BitcoinLikeOperation(
+                const std::shared_ptr<BitcoinLikeOperation>& operation,
+                BitcoinLikeBlockchainExplorerTransaction const& tx);
+
+            std::shared_ptr<api::BitcoinLikeTransaction> getTransaction() override;
+
+            void refreshUid(std::string const& additional = "") override;
+            bool isComplete() override;
+
+            void setExplorerTransaction(BitcoinLikeBlockchainExplorerTransaction const& tx);
+            BitcoinLikeBlockchainExplorerTransaction& getExplorerTransaction();
+
+        private:
+            std::shared_ptr<api::BitcoinLikeTransaction> _tx;
+            BitcoinLikeBlockchainExplorerTransaction _explorerTx;
+        };
+    }
+}
---
+++ ledger-core-bitcoin/inc/bitcoin/operations/BitcoinLikeOperationQuery.hpp
@@ -0,0 +1,58 @@
+/*
+ *
+ * BitcoinLikeOperationQuery
+ *
+ * Created by Alexis Le Provost on 16/01/2020.
+ *
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2020 Ledger
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
+#include <soci.h>
+
+#include <core/api/QueryFilter.hpp>
+#include <core/operation/OperationQuery.hpp>
+
+#include <bitcoin/operations/BitcoinLikeOperation.hpp>
+
+namespace ledger {
+    namespace core {
+        class BitcoinLikeOperationQuery : public OperationQuery<BitcoinLikeOperation> {
+        public:
+         BitcoinLikeOperationQuery(
+                const std::shared_ptr<api::QueryFilter>& headFilter,
+                const std::shared_ptr<DatabaseSessionPool>& pool,
+                const std::shared_ptr<api::ExecutionContext>& context,
+                const std::shared_ptr<api::ExecutionContext>& mainContext);
+
+        protected:
+            virtual void inflateCompleteTransaction(
+                soci::session &sql, const std::string &accountUid, BitcoinLikeOperation& operation) override;
+
+            virtual std::shared_ptr<BitcoinLikeOperation> createOperation(
+                std::shared_ptr<AbstractAccount> &account) override;
+        };
+
+
+    }
+}
