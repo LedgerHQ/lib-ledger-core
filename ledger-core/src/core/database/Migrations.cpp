0a4f398f18718b9507e808464633e0ad9ad0da77
Dimitri Sabadie <dimitri.sabadie@gmail.com>
2020-01-22 16:43:50 +0100

    Fix the units migration.
    We had an extra column.

--- ledger-core/src/core/database/Migrations.cpp
+++ ledger-core/src/core/database/Migrations.cpp
@@ -59,7 +59,6 @@ namespace ledger {
             sql << "CREATE TABLE units("
                 "name VARCHAR(255) NOT NULL,"
                 "magnitude INTEGER NOT NULL,"
-                "symbol VARCHAR(255) NOT NULL,"
                 "code VARCHAR(255) NOT NULL,"
                 "currency_name VARCHAR(255) REFERENCES currencies(name) ON DELETE CASCADE ON UPDATE CASCADE"
             ")";
