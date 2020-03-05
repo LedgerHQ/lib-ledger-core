fc2259063b6db23b97e11bd124ebe69daab3a47e
Dimitri Sabadie <dimitri.sabadie@gmail.com>
2020-03-05 18:09:06 +0100

    Fix find_dependency() in the ledger-core-config.cmake file.

--- ledger-core/ledger-core-config.cmake
+++ ledger-core/ledger-core-config.cmake
@@ -1,6 +1,13 @@
 include(CMakeFindDependencyMacro)
-find_dependency(Qt5 COMPONENTS Core Widgets Network Gui Concurrent WebSockets)
+
+if (BUILD_TESTS)
+    find_dependency(Qt5 COMPONENTS Core Widgets Network Gui Concurrent WebSockets)
+endif()
 find_dependency(Threads)
+
+if (SYS_OPENSSL)
+    find_dependency(OpenSSL)
+endif()
 
 get_filename_component(SELF_DIR "${CMAKE_CURRENT_LIST_FILE}" DIRECTORY)
 include(${SELF_DIR}/ledger-core.cmake)
