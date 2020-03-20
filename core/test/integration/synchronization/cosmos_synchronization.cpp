3a0ad1209742d91e7e81a6a706ae2061f60ff636
haammar-ledger <hakim.aammar-ext@ledger.fr>
2020-03-20 18:54:56 +0100

    Fix Cosmos Synchronization tests following rebase

--- core/test/integration/synchronization/cosmos_synchronization.cpp
+++ core/test/integration/synchronization/cosmos_synchronization.cpp
@@ -79,8 +79,8 @@ public:
     }
 
     void setupTest(std::shared_ptr<WalletPool>& pool,
-                   std::shared_ptr<AbstractWallet>& wallet,
                    std::shared_ptr<CosmosLikeAccount>& account,
+                   std::shared_ptr<AbstractWallet>& wallet,
                    const std::string& pubKey) {
 
 #ifdef PG_SUPPORT
@@ -340,10 +340,10 @@ TEST_F(CosmosLikeWalletSynchronization, Balances)
         "0388459b2653519948b12492f1a0b464720110c147a8155d23d423a5cc3c21d89a";  // Obelix
 
     std::shared_ptr<WalletPool> pool;
-    std::shared_ptr<AbstractWallet> wallet;
     std::shared_ptr<CosmosLikeAccount> account;
+    std::shared_ptr<AbstractWallet> wallet;
 
-    setupTest(pool, wallet, account, hexPubKey);
+    setupTest(pool, account, wallet, hexPubKey);
 
     const std::string address = account->getKeychain()->getAddress()->toBech32();
     const std::string mintscanExplorer = fmt::format("https://www.mintscan.io/account/{}", address);
@@ -384,10 +384,10 @@ TEST_F(CosmosLikeWalletSynchronization, AllTransactionsSynchronization) {
     std::string hexPubKey = "0388459b2653519948b12492f1a0b464720110c147a8155d23d423a5cc3c21d89a"; // Obelix
 
     std::shared_ptr<WalletPool> pool;
-    std::shared_ptr<AbstractWallet> wallet;
     std::shared_ptr<CosmosLikeAccount> account;
+    std::shared_ptr<AbstractWallet> wallet;
 
-    setupTest(pool, wallet, account, hexPubKey);
+    setupTest(pool, account, wallet, hexPubKey);
 
     performSynchro(account);
 
@@ -451,34 +451,6 @@ TEST_F(CosmosLikeWalletSynchronization, AllTransactionsSynchronization) {
     EXPECT_TRUE(foundMsgWithdrawDelegatorReward);
     EXPECT_TRUE(foundMsgWithdrawValidatorCommission);
     EXPECT_TRUE(foundMsgUnjail);
-}
-
-
-TEST_F(CosmosLikeWalletSynchronization, SuccessiveSynchronizations) {
-    std::string hexPubKey(ledger::testing::cosmos::DEFAULT_HEX_PUB_KEY);
-
-    std::shared_ptr<WalletPool> pool;
-    std::shared_ptr<AbstractWallet> wallet;
-    std::shared_ptr<CosmosLikeAccount> account;
-
-    setupTest(pool, wallet, account, hexPubKey);
-
-    // First synchro
-    performSynchro(account);
-    auto blockHeight1 = wait(account->getLastBlock()).height;
-
-    // Wait 30s (new Cosmos block every 7s)
-    fmt::print("Waiting new Cosmos block for 30s...\n");
-    std::flush(std::cerr);
-    std::flush(std::cout);
-    std::this_thread::sleep_for(std::chrono::seconds(30));
-
-    // Second synchro
-    // FIXME Fails due to limitation of test framework??
-    performSynchro(account);
-    auto blockHeight2 = wait(account->getLastBlock()).height;
-
-    EXPECT_NE(blockHeight1, blockHeight2);
 }
 
 TEST_F(CosmosLikeWalletSynchronization, ValidatorSet) {
@@ -555,10 +527,41 @@ TEST_F(CosmosLikeWalletSynchronization, BalanceHistoryOperationQuery) {
     std::string hexPubKey = "0388459b2653519948b12492f1a0b464720110c147a8155d23d423a5cc3c21d89a"; // Obelix
 
     std::shared_ptr<WalletPool> pool;
-    std::shared_ptr<AbstractWallet> wallet;
     std::shared_ptr<CosmosLikeAccount> account;
+    std::shared_ptr<AbstractWallet> wallet;
 
-    setupTest(pool, wallet, account, hexPubKey);
+    setupTest(pool, account, wallet, hexPubKey);
+
+    performSynchro(account);
+
+    const auto &uid = account->getAccountUid();
+    soci::session sql(wallet->getDatabase()->getPool());
+    std::vector<Operation> operations;
+
+    auto keychain = account->getKeychain();
+    std::function<bool(const std::string &)> filter = [&keychain](const std::string addr) -> bool {
+        return keychain->contains(addr);
+    };
+
+    //Get operations related to an account
+    CosmosLikeOperationDatabaseHelper::queryOperations(
+        sql,
+        uid,
+        operations,
+        filter);
+
+    ASSERT_GE(operations.size(), 17) << "As of 2020-03-19, there are 17 operations picked up by the query";
+}
+
+TEST_F(CosmosLikeWalletSynchronization, GetAccountDelegations) {
+
+    std::string hexPubKey = "0388459b2653519948b12492f1a0b464720110c147a8155d23d423a5cc3c21d89a"; // Obelix
+
+    std::shared_ptr<WalletPool> pool;
+    std::shared_ptr<CosmosLikeAccount> account;
+    std::shared_ptr<AbstractWallet> wallet;
+
+    setupTest(pool, account, wallet, hexPubKey);
 
     auto delegations = wait(account->getDelegations());
     EXPECT_GE(delegations.size(), 2);
@@ -576,10 +579,10 @@ TEST_F(CosmosLikeWalletSynchronization, GetAccountPendingRewards) {
     std::string hexPubKey = "0388459b2653519948b12492f1a0b464720110c147a8155d23d423a5cc3c21d89a"; // Obelix
 
     std::shared_ptr<WalletPool> pool;
-    std::shared_ptr<AbstractWallet> wallet;
     std::shared_ptr<CosmosLikeAccount> account;
+    std::shared_ptr<AbstractWallet> wallet;
 
-    setupTest(pool, wallet, account, hexPubKey);
+    setupTest(pool, account, wallet, hexPubKey);
 
     auto rewards = wait(account->getPendingRewards());
     EXPECT_GE(rewards.size(), 2);
@@ -591,3 +594,32 @@ TEST_F(CosmosLikeWalletSynchronization, GetAccountPendingRewards) {
     EXPECT_GE(pendingReward.toUint64(), 1000UL); // 1000 uATOM
 
 }
+
+// FIXME This test fails ; put at the end because it also messes up the other tests
+TEST_F(CosmosLikeWalletSynchronization, SuccessiveSynchronizations) {
+    std::string hexPubKey(ledger::testing::cosmos::DEFAULT_HEX_PUB_KEY);
+
+    std::shared_ptr<WalletPool> pool;
+    std::shared_ptr<CosmosLikeAccount> account;
+    std::shared_ptr<AbstractWallet> wallet;
+
+    setupTest(pool, account, wallet, hexPubKey);
+
+    // First synchro
+    performSynchro(account);
+    auto blockHeight1 = wait(account->getLastBlock()).height;
+
+    // Wait 30s (new Cosmos block every 7s)
+    fmt::print("Waiting new Cosmos block for 30s...\n");
+    std::flush(std::cerr);
+    std::flush(std::cout);
+    std::this_thread::sleep_for(std::chrono::seconds(30));
+
+    // Second synchro
+    // FIXME Fails due to limitation of test framework??
+    performSynchro(account);
+    auto blockHeight2 = wait(account->getLastBlock()).height;
+
+    EXPECT_NE(blockHeight1, blockHeight2);
+}
+
