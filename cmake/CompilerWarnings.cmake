a24effbb50a4e8b5a6a7bfa843af9d9e5e169114
Alexis Le Provost <alexis.le-provost@ledger.fr>
2020-03-26 14:21:27 +0100

    [core] enable compiler warnings for core and coin libraries

--- cmake/CompilerWarnings.cmake
+++ cmake/CompilerWarnings.cmake
@@ -1,15 +1,16 @@
-function(enable_warnings target_name)
+function(toggle_compiler_warnings target_name)
     option(ENABLE_WARNINGS "Enable compiler warnings" OFF)
+    option(WARNINGS_AS_ERRORS "Treat warnings as erros" OFF)
 
-    if (ENABLE_WARNINGS)
+    if(ENABLE_WARNINGS)
         set(MSVC_WARNINGS
             /W4 # Baseline reasonable warnings
-            /w14242 # 'identfier': conversion from 'type1' to 'type1', possible loss of data
+            /w14242 # 'identifier': conversion from 'type1' to 'type1', possible loss of data
             /w14254 # 'operator': conversion from 'type1:field_bits' to 'type2:field_bits', possible loss
                     # of data
-            /w14263 # 'function': member function does not override any base class virtual member function
-            /w14265 # 'classname': class has virtual functions, but destructor is not virtual instances of
-                    # this class may not be destructed correctly
+            /w14263 # 'function': member function does not override any virtual member function of base class
+            /w14265 # 'classname': class has virtual functions, but destructor is not a virtual instance of
+                    # this class; it may not be destructed correctly
             /w14287 # 'operator': unsigned/negative constant mismatch
             /we4289 # nonstandard extension used: 'variable': loop control variable declared in the for-
                     # loop is used outside the for-loop scope
@@ -38,16 +39,20 @@ function(enable_warnings target_name)
                                 # destructor. This helps catch hard to track down memory errors
             -Wold-style-cast # warn for c-style casts
             -Wcast-align # warn for potential performance problem casts
-            -Wunused # warn on anything being unused
             -Woverloaded-virtual # warn if you overload (not override) a virtual function
             -Wpedantic # warn if non-standard C++ is used
             -Wconversion # warn on type conversions that may lose data
-            -Wsign-conversion # warn on sign conversions
             -Wnull-dereference # warn if a null dereference is detected
             -Wdouble-promotion # warn if float is implicit promoted to double
             -Wformat=2 # warn on security issues around functions that format output (ie printf)
         )
         
+        if(WARNINGS_AS_ERRORS)
+            message("Treat compiler warnings as errors")
+            set(CLANG_WARNINGS ${CLANG_WARNINGS} -Werror)
+            set(MSVC_WARNINGS ${MSVC_WARNINGS} /WX)
+        endif()
+
         if(CMAKE_BUILD_TYPE STREQUAL "Debug")
             set(CLANG_WARNINGS ${CLANG_WARNINGS} -O0 -g)
             set(MSVC_WARNINGS ${MSVC_WARNINGS} /Od)
@@ -64,7 +69,6 @@ function(enable_warnings target_name)
 
         set(GCC_WARNINGS
             ${CLANG_WARNINGS}
-            -Wmisleading-indentation # warn if identation implies blocks where blocks do not exist
             -Wduplicated-cond # warn if if / else chain has duplicated conditions
             -Wduplicated-branches # warn if if / else branches have duplicated code
             -Wlogical-op # warn about logical operations being used where bitwise were probably wanted
@@ -72,12 +76,18 @@ function(enable_warnings target_name)
         )
 
         target_compile_options(
-            ${target_name}
-            INTERFACE $<$<OR:$<CXX_COMPILER_ID:Clang>,$<CXX_COMPILER_ID:AppleClang>>:
-                    ${CLANG_WARNINGS}>
-                    $<$<CXX_COMPILER_ID:MSVC>:
-                    ${MSVC_WARNINGS}>
-                    $<$<CXX_COMPILER_ID:GNU>:
-                    ${GCC_WARNINGS}>)
+            ${target_name} PRIVATE
+              $<$<OR:$<CXX_COMPILER_ID:Clang>,$<CXX_COMPILER_ID:AppleClang>>: ${CLANG_WARNINGS}>
+              $<$<CXX_COMPILER_ID:MSVC>: ${MSVC_WARNINGS}>
+              $<$<CXX_COMPILER_ID:GNU>: ${GCC_WARNINGS}>)
     endif()
+endfunction()
+
+function(disable_compiler_warnings target_name)
+    target_compile_options(
+        ${target_name} PRIVATE
+            $<$<CXX_COMPILER_ID:MSVC>: /w>
+            $<$<OR:$<CXX_COMPILER_ID:Clang>,$<CXX_COMPILER_ID:AppleClang>>: -Wno-everything>
+            $<$<CXX_COMPILER_ID:GNU>: -w>
+    )
 endfunction()
