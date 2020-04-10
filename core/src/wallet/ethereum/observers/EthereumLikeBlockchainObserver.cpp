7497a922ced9fd1aaf91eea6bdf56df18fb4dea9
KhalilBellakrid <khalil.bellakrid@ledger.fr>
2020-04-10 10:28:04 +0200

    Take into account PR reviews

--- core/src/wallet/ethereum/observers/EthereumLikeBlockchainObserver.cpp
+++ core/src/wallet/ethereum/observers/EthereumLikeBlockchainObserver.cpp
@@ -55,16 +55,10 @@ namespace ledger {
             std::lock_guard<std::mutex> lock(_lock);
             for (const auto& account : _accounts) {
                 account->run([account, tx] () {
-                    bool shouldEmitNow = false;
-                    {
-                        soci::session sql(account->getWallet()->getDatabase()->getPool());
-                        soci::transaction tr(sql);
-                        shouldEmitNow = account->putTransaction(sql, tx) != EthereumLikeAccount::FLAG_TRANSACTION_IGNORED;
-                        tr.commit();
-                    }
-                    if (shouldEmitNow) {
-                        account->emitEventsNow();
-                    }
+                    EthereumBlockchainObserver::emitEvent(account, [tx](soci::session &sql,
+                                                                       const std::shared_ptr<EthereumLikeAccount> &acc) {
+                        return acc->putTransaction(sql, tx) != EthereumLikeAccount::FLAG_TRANSACTION_IGNORED;
+                    });
                 });
             }
         }
@@ -73,16 +67,10 @@ namespace ledger {
             std::lock_guard<std::mutex> lock(_lock);
             for (const auto& account : _accounts) {
                 account->run([account, block] () {
-                    bool shouldEmitNow = false;
-                    {
-                        soci::session sql(account->getWallet()->getDatabase()->getPool());
-                        soci::transaction tr(sql);
-                        shouldEmitNow = account->putBlock(sql, block);
-                        tr.commit();
-                    }
-                    if (shouldEmitNow) {
-                        account->emitEventsNow();
-                    }
+                    EthereumBlockchainObserver::emitEvent(account, [block](soci::session &sql,
+                                                                          const std::shared_ptr<EthereumLikeAccount> &acc) {
+                        return acc->putBlock(sql, block);
+                    });
                 });
             }
         }
