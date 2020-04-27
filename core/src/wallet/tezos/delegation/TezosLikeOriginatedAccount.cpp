9b885a036c289310eae220e61e0325446f7dac90
Dimitri Sabadie <dimitri.sabadie@gmail.com>
2020-04-27 15:34:38 +0200

    LLC-643 – Fix negative / non-matching balances on Tezos (originated accounts).
    The problem came from the Babylon 2.0 protocol update, which introduced
    a new way to spend funds and pay fees. The code is pretty
    self-explanatory, so feel free to read the content of the commit for
    further information, but basically, a mirror fix is needed on the
    implicit accounts, as they now have to spend the fees of originated
    accounts as well.

--- core/src/wallet/tezos/delegation/TezosLikeOriginatedAccount.cpp
+++ core/src/wallet/tezos/delegation/TezosLikeOriginatedAccount.cpp
@@ -32,14 +32,24 @@
 #include "TezosLikeOriginatedAccount.h"
 #include <database/query/ConditionQueryFilter.h>
 #include <api/ErrorCode.hpp>
+#include <limits>
 #include <wallet/currencies.hpp>
 #include <wallet/common/BalanceHistory.hpp>
 #include <api/Operation.hpp>
 #include <api/OperationOrderKey.hpp>
+#include <api/TezosLikeOperation.hpp>
+#include <api/TezosLikeTransaction.hpp>
 #include <wallet/pool/WalletPool.hpp>
 
 namespace ledger {
     namespace core {
+        // we need to compare the block height to the Babylon update height activation
+        // because the way balances are computed difffers from pre- and post- Babylon
+        // protocol activation
+        //
+        // <https://tezos.foundation/update-week-of-14-october-2019>
+        const int64_t BABYLON_2_0_ACTIVATION_BLOCK_HEIGHT = 655361;
+
         TezosLikeOriginatedAccount::TezosLikeOriginatedAccount(const std::string &uid,
                                                                const std::string &address,
                                                                const std::shared_ptr<TezosLikeAccount> &originatorAccount,
@@ -82,19 +92,47 @@ namespace ledger {
                 for (auto &op : ops) {
                     auto ty = op->getOperationType();
                     auto value = BigInt(op->getAmount()->toString());
+                    auto tzOp = op->asTezosLikeOperation();
+                    auto tzTx = tzOp->getTransaction();
+
                     switch (ty) {
-                        case api::OperationType::RECEIVE:
-                            result = result + value;
+                        case api::OperationType::RECEIVE: {
+                            // we discard received transactions that have failed
+                            if (tzTx->getStatus() == 1) {
+                                result = result + value;
+                            }
+
                             break;
+                        }
+
                         case api::OperationType::SEND: {
-                            auto fees = BigInt(op->getFees()->toString());
-                            result = result - (value + fees);
+                            // if sending a transaction has failed:
+                            //
+                            // - Then we only spend the fees pre- Babylon 2.0.
+                            // - We do nothing post- Babylon 2.0 as the fees will be spent by the
+                            //   parent account.
+                            auto isPreBabylon = op->getBlockHeight().value_or(std::numeric_limits<int64_t>::max()) < BABYLON_2_0_ACTIVATION_BLOCK_HEIGHT;
+
+                            if (tzTx->getStatus() == 0) {
+                                value = BigInt::ZERO;
+                            }
+
+                            if (isPreBabylon) {
+                                auto fees = BigInt(op->getFees()->toString());
+                                result = result - (value + fees);
+                            } else {
+                                // we do not spend the fees, as they are spent by the parent
+                                // account
+                                result = result - value;
+                            }
+
                             break;
                         }
                         default:
                             break;
                     }
                 }
+
                 return std::make_shared<Amount>(currencies::TEZOS, 0, result);
             });
         }
@@ -131,14 +169,39 @@ namespace ledger {
 
                     static inline void update_balance(std::shared_ptr<api::Operation>& op, BigInt& sum) {
                         auto value = BigInt(op->getAmount()->toString());
+                        auto tzOp = op->asTezosLikeOperation();
+                        auto tzTx = tzOp->getTransaction();
+
                         switch (op->getOperationType()) {
-                            case api::OperationType::RECEIVE:
-                                sum = sum + value;
+                            case api::OperationType::RECEIVE: {
+                                // we discard received transactions that have failed
+                                if (tzTx->getStatus() == 1) {
+                                  sum = sum + value;
+                                }
                                 break;
+                            }
 
                             case api::OperationType::SEND: {
-                                auto fees = BigInt(op->getFees()->toString());
-                                sum = sum - (value + fees);
+                                // if sending a transaction has failed:
+                                //
+                                // - Then we only spend the fees pre- Babylon 2.0.
+                                // - We do nothing post- Babylon 2.0 as the fees will be spent by the
+                                //   parent account.
+                                auto isPreBabylon = op->getBlockHeight().value_or(std::numeric_limits<int64_t>::max()) < BABYLON_2_0_ACTIVATION_BLOCK_HEIGHT;
+
+                                if (tzTx->getStatus() == 0) {
+                                  value = BigInt::ZERO;
+                                }
+
+                                if (isPreBabylon) {
+                                  auto fees = BigInt(op->getFees()->toString());
+                                  sum = sum - (value + fees);
+                                } else {
+                                  // we do not spend the fees, as they are spent by the parent
+                                  // account
+                                  sum = sum - value;
+                                }
+
                                 break;
                             }
 
