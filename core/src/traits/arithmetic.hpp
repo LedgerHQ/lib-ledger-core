5b13817b1252d23f7b462781539825066475830a
Pierre Pollastri <pierre@ledger.fr>
2020-05-05 10:59:15 +0200

    Fix BigInt::assignScalar for unsigned types

---
+++ core/src/traits/arithmetic.hpp
@@ -0,0 +1,49 @@
+/*
+ *
+ * arithmetic.hpp
+ * ledger-core
+ *
+ * Created by Pierre Pollastri on 07/05/2020.
+ *
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2020 Ledger
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
+#ifndef LEDGER_CORE_ARITHMETIC_HPP
+#define LEDGER_CORE_ARITHMETIC_HPP
+
+#include <type_traits>
+
+namespace ledger {
+    namespace core {
+
+        template <typename T>
+        using isSigned = std::enable_if_t<std::is_signed<T>::value, bool>;
+
+        template <typename T>
+        using isUnsigned = std::enable_if_t<std::is_unsigned<T>::value, bool>;
+
+    }
+}
+
+#endif //LEDGER_CORE_ARITHMETIC_HPP
