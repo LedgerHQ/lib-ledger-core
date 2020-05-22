dbadbbc4b69b9fa6b85df89f38ac7ac5d2b4d198
Dimitri Sabadie <dimitri.sabadie@gmail.com>
2020-05-22 11:28:58 +0200

    Migrate the node binding to the new modularized core.

--- tools/idl_pkg.sh
+++ tools/idl_pkg.sh
@@ -37,77 +37,46 @@ function generate_npm_interface {
     mkdir -p $NODE_DIR/js/tmp
   fi
 
-  for coin in $*; do
-    if [ "$coin" == "core" ]; then
-      PROJECT_NAME=ledger-core
-      NODE_PKG_NAME=ledgercore
-    else
-      PROJECT_NAME=ledger-core-$coin
-      NODE_PKG_NAME=ledgercore-$coin
-    fi
+  API_DIR=./bundle/api
+  BUILD_DIR=${BUILD_DIR:-build}
+  NODE_PKG_NAME=ledgercore
 
-    CURRENCY_NAME=$coin
-    API_DIR=$PROJECT_NAME/inc/$CURRENCY_NAME/api
-    CPP_JNI_DIR=$PROJECT_NAME/inc/$CURRENCY_NAME/jni
-    BUILD=$PROJECT_NAME/build
+  echo -e "Generating Node binding code"
 
-    echo -e "Generating $PROJECT_NAME ($CURRENCY_NAME) JS binding code"
+  ./djinni/src/run \
+    --idl ./bundle/bundle.djinni \
+    --cpp-out $API_DIR \
+    --cpp-namespace ledger::core::api \
+    --cpp-optional-template std::experimental::optional \
+    --cpp-optional-header "\"utils/Optional.hpp\"" \
+    --node-out $NODE_SRC_DIR \
+    --node-type-prefix NJS \
+    --node-include-cpp "../include" \
+    --node-package $NODE_PKG_NAME \
+    --export-header-name libcore_export \
+    --trace $trace
 
-    ./djinni/src/run \
-      --idl $PROJECT_NAME/idl/idl.djinni \
-      --cpp-out $API_DIR \
-      --cpp-namespace ledger::core::api \
-      --cpp-optional-template std::experimental::optional \
-      --cpp-optional-header "\"utils/Optional.hpp\"" \
-      --node-out $NODE_SRC_DIR \
-      --node-type-prefix NJS \
-      --node-include-cpp "../include" \
-      --node-package $NODE_PKG_NAME \
-      --export-header-name libcore_export \
-      --trace $trace
+  # copy include files
+  echo "Copying header files…"
+  cp -r $API_DIR/* $NODE_DIR/include
 
-    # copy include files
-    echo "Copying header files…"
-    cp -r $API_DIR/* $NODE_DIR/include
+  # copy util files
+  echo "Copying utils files…"
+  cp -r ledger-core/inc/core/utils/Optional.hpp $NODE_DIR/include/utils
+  cp -r ledger-core/inc/core/LibCoreExport.hpp $NODE_DIR/include
 
-    # copy util files
-    echo "Copying utils files…"
-    cp -r ledger-core/inc/core/utils/Optional.hpp $NODE_DIR/include/utils
-    cp -r ledger-core/inc/core/LibCoreExport.hpp $NODE_DIR/include
+  # copy lib files
+  echo "Copying lib files"
+  mkdir -p $NODE_DIR/lib
 
-    # copy lib files
-    echo "Copying lib files"
-    mkdir -p $NODE_DIR/lib
-
-    # copy dynamic library
-    echo "Copying the dynamic library"
-    cp $BUILD/src/lib$PROJECT_NAME.* $NODE_DIR/lib
-  done
-
-  # Gather all #include
-  echo "Generating the final output C++ one-source concatenated interface"
-  OUTPUT_CPP=$NODE_SRC_DIR/final-output.cpp
-
-  (find $NODE_SRC_DIR -type f -name "ledgercore*cpp" -exec grep "\#include" '{}' \;) > $OUTPUT_CPP
-  echo "using namespace v8;" >> $OUTPUT_CPP
-  echo "using namespace node;" >> $OUTPUT_CPP
-  echo "static void initAll(Local<Object> target) {" >> $OUTPUT_CPP
-  echo -e "    Nan::HandleScope scope;" >> $OUTPUT_CPP
-  (find $NODE_SRC_DIR -type f -name "ledgercore*cpp" -exec grep "Initialize(target)" '{}' \; | awk '!x[$0]++') >> $OUTPUT_CPP
-  echo "}" >> $OUTPUT_CPP
-  echo "NODE_MODULE(ledgercore,initAll);" >> $OUTPUT_CPP
-
-  echo "Removing temporary C++ source interface"
-  rm $NODE_SRC_DIR/ledgercore*cpp
-
-  echo "Generating the binding.gyp file…"
-  ./tools/gyp_generator.py $*
+  # copy dynamic library
+  echo "Copying the dynamic library"
+  cp $BUILD_DIR/libledger-core-bundle.* $NODE_DIR/lib
 }
 
 case "$1" in
   "npm")
-    shift
-    generate_npm_interface $*
+    generate_npm_interface
     ;;
 
   *)
