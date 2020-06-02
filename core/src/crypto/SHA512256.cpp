cb4b9ead60294bc84d81774a9a04df7d01b1a720
llehmann-ledger <65655263+llehmann-ledger@users.noreply.github.com>
2020-06-02 10:58:31 +0200

    Merge PR

--- core/src/crypto/SHA512256.cpp
+++ core/src/crypto/SHA512256.cpp
@@ -29,45 +29,63 @@
 
 
 #include "SHA512256.hpp"
-#include <utils/hex.h>
+#include <utils/Exception.hpp>
+#include <utils/hex.hpp>
+#include <math/BaseConverter.hpp>
 
 #include <openssl/sha.h>
 
+
 namespace ledger {
-    namespace core {
-        std::string SHA512256::stringToHexHash(const std::string &input) {
-            return hex::toString(stringToBytesHash(input));
-        }
+namespace core {
+namespace algorand {
 
-        std::string SHA512256::bytesToHexHash(const std::vector<uint8_t> &bytes) {
-            return hex::toString(bytesToBytesHash(bytes));
-        }
+    // Stolen from OpenSSL 1.1.1:
+    // https://github.com/openssl/openssl/blob/4bed94f0c11ef63587c6b2edb03c3c438e221604/crypto/sha/sha512.c#L81
+    static void sha512_256_init(SHA512_CTX *c)
+    {
+        c->h[0] = U64(0x22312194fc2bf72c);
+        c->h[1] = U64(0x9f555fa3c84c64c2);
+        c->h[2] = U64(0x2393b86b6f53b151);
+        c->h[3] = U64(0x963877195940eabd);
+        c->h[4] = U64(0x96283ee2a88effe3);
+        c->h[5] = U64(0xbe5e1e2553863992);
+        c->h[6] = U64(0x2b0199fc2c85b8aa);
+        c->h[7] = U64(0x0eb72ddc81c52ca2);
 
-        std::vector<uint8_t> SHA512256::dataToBytesHash(const void *data, size_t size) {
-            /*
-            // FIXME Use SHA-512/256 instead of SHA-512
-            uint8_t hash[SHA512256_DIGEST_LENGTH];
-            SHA512256_CTX sha512;
-            SHA512256_Init(&sha512);
-            SHA512256_Update(&sha512, data, size);
-            SHA512256_Final(hash, &sha512);
-            return std::vector<uint8_t >(hash, hash + SHA512_DIGEST_LENGTH);
-            /*/
-            uint8_t hash[SHA512_DIGEST_LENGTH];
-            SHA512_CTX sha512;
-            SHA512_Init(&sha512);
-            SHA512_Update(&sha512, data, size);
-            SHA512_Final(hash, &sha512);
-            return std::vector<uint8_t >(hash, hash + SHA512_DIGEST_LENGTH);
-            //*/
-        }
-
-        std::vector<uint8_t> SHA512256::stringToBytesHash(const std::string &input) {
-            return dataToBytesHash(input.c_str(), input.size());
-        }
-
-        std::vector<uint8_t> SHA512256::bytesToBytesHash(const std::vector<uint8_t> &bytes) {
-            return dataToBytesHash(bytes.data(), bytes.size());
-        }
+        c->Nl = 0;
+        c->Nh = 0;
+        c->num = 0;
+        c->md_len = SHA256_DIGEST_LENGTH;
     }
+
+    std::string SHA512256::stringToHexHash(const std::string &input) {
+        return hex::toString(stringToBytesHash(input));
+    }
+
+    std::string SHA512256::bytesToHexHash(const std::vector<uint8_t> &bytes) {
+        return hex::toString(bytesToBytesHash(bytes));
+    }
+
+    std::vector<uint8_t> SHA512256::stringToBytesHash(const std::string &input) {
+        return dataToBytesHash(reinterpret_cast<const uint8_t*>(input.c_str()), input.size());
+    }
+
+    std::vector<uint8_t> SHA512256::bytesToBytesHash(const std::vector<uint8_t> &bytes) {
+        return dataToBytesHash(bytes.data(), bytes.size());
+    }
+
+    std::vector<uint8_t> SHA512256::dataToBytesHash(const uint8_t *data, size_t size) {
+
+        uint8_t hash[SHA256_DIGEST_LENGTH];
+        SHA512_CTX sha512;
+        sha512_256_init(&sha512);
+        SHA512_Update(&sha512, data, size);
+        SHA512_Final(hash, &sha512);
+        return std::vector<uint8_t >(hash, hash + SHA256_DIGEST_LENGTH);
+
+    }
+
+}
+}
 }
