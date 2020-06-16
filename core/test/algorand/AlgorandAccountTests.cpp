d86657ab07a7a99694b5ad21aa5e0e4453a55198
llehmann-ledger <65655263+llehmann-ledger@users.noreply.github.com>
2020-06-16 12:20:08 +0200

    Create new accounts with the account public key

--- core/test/algorand/AlgorandAccountTests.cpp
+++ core/test/algorand/AlgorandAccountTests.cpp
@@ -204,7 +204,7 @@ public:
         const auto currency = currencies::ALGORAND;
         registerCurrency(currency);
 
-         accountInfo = api::AccountCreationInfo(1, {}, {}, { { std::begin(OBELIX), std::end(OBELIX) } }, {}); // TODO: FIXEME
+        accountInfo = api::AccountCreationInfo(1, {}, {}, { algorand::Address::toPublicKey(OBELIX) }, {});
 
         wallet = std::dynamic_pointer_cast<algorand::Wallet>(wait(pool->createWallet("algorand", currency.name, api::DynamicObject::newInstance())));
         account = createAlgorandAccount(wallet, accountInfo.index, accountInfo);
