5e132023cf71def9dc61c2e436d01e5a2cbb30b1
Pierre Pollastri <pierre@ledger.fr>
2020-06-25 23:00:29 +0200

    Replace Javascript module to get libcore by shell script

--- .circleci/export_lib_version.sh
+++ .circleci/export_lib_version.sh
@@ -13,6 +13,11 @@ LIB_VERSION_MINOR=`cat CMakeLists.txt | match 's/set\(VERSION_MINOR.*([0-9]+).*\
 LIB_VERSION_PATCH=`cat CMakeLists.txt | match 's/set\(VERSION_PATCH.*([0-9]+).*\)/\1/p'`
 LIB_VERSION=$LIB_VERSION_MAJOR.$LIB_VERSION_MINOR.$LIB_VERSION_PATCH
 
+if [ -z "$CIRCLE_TAG" ]; then
+	COMMIT_HASH=`echo $CIRCLE_SHA1 | cut -c 1-6`
+	LIB_VERSION="$LIB_VERSION-rc-$COMMIT_HASH"
+fi
+
 echo "export LIB_VERSION=$LIB_VERSION" >> $BASH_ENV
 
 echo "=====> Libcore version"
