3fc60e0461c86739b02cb914bf6d96fc1886ae5b
jdellinger <jeremy.dellinger@ledger.com>
2020-06-25 16:38:54 +0200

    [ci] validate branch deploy exec

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -5,7 +5,7 @@ if [ -n "$CIRCLE_TAG" ] || [ "$CIRCLE_BRANCH" == "master" -o "$CIRCLE_BRANCH" ==
 	aws s3 sync ./ s3://ledger-lib-ledger-core/$LIB_VERSION/ --acl public-read && \
 	aws s3 ls s3://ledger-lib-ledger-core/$LIB_VERSION;
 	cd -
-elif [ "$CIRCLE_BRANCH" =~ ^int-.* ]; then
+elif [[ "$CIRCLE_BRANCH" =~ ^int-.* ]]; then
 	cd ../lib-ledger-core-artifacts
 	ls -la
     if [ -z "$1" ]
