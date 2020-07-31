b60bae3b25fc19def943cc5fc2067b7ae02181b5
haammar-ledger <hakim.aammar-ext@ledger.fr>
2020-07-31 17:58:20 +0200

    Update Algorand reverse proxy endpoints for libcore and tests

--- core/test/algorand/AlgorandSynchronizationTests.cpp
+++ core/test/algorand/AlgorandSynchronizationTests.cpp
@@ -57,7 +57,8 @@ public:
         auto configuration = DynamicObject::newInstance();
         configuration->putString(api::Configuration::KEYCHAIN_DERIVATION_SCHEME,"44'/<coin_type>'/<account>'/<node>'/<address>");
         configuration->putString(api::Configuration::BLOCKCHAIN_EXPLORER_ENGINE, api::AlgorandBlockchainExplorerEngines::ALGORAND_NODE);
-        configuration->putString(api::Configuration::BLOCKCHAIN_EXPLORER_API_ENDPOINT, "https://testnet-algorand.api.purestake.io"); // NOTE: We run Algorand tests on the TestNet
+        // NOTE: we run the tests on the staging environment which is on the TestNet
+        configuration->putString(api::Configuration::BLOCKCHAIN_EXPLORER_API_ENDPOINT, "https://algorand.coin.staging.aws.ledger.com");
 
         auto wallet = std::dynamic_pointer_cast<Wallet>(wait(pool->createWallet("test-wallet", "algorand", configuration)));
 
