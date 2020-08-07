96fdb5f1be67d93bf02179a7e865bf1a67ea8569
Habib LAFET <habib.lafet-ext@ledger.fr>
2020-08-07 11:09:31 +0200

    fix bitcoin utxo picker

--- core/src/wallet/bitcoin/transaction_builders/BitcoinLikeStrategyUtxoPicker.cpp
+++ core/src/wallet/bitcoin/transaction_builders/BitcoinLikeStrategyUtxoPicker.cpp
@@ -564,6 +564,7 @@ namespace ledger {
             pickedUtxos.reserve(utxos.size());
             std::sort(utxos.begin(), utxos.end(), functor);
 
+            bool enough = false;
             for (auto const &u : utxos) {
                 amount = amount + *u.value.value();
                 pickedInputs += 1;
@@ -573,11 +574,12 @@ namespace ledger {
 
                 auto const computeOutputAmount = pickedInputs == utxos.size();
                 if (hasEnough(buddy, amount, pickedInputs, currency, computeOutputAmount)) {
+                    enough = true;
                     break;
                 }
             }
 
-            if (pickedInputs >= utxos.size() && !buddy->request.wipe) {
+            if (!enough && !buddy->request.wipe) {
                 throw make_exception(api::ErrorCode::NOT_ENOUGH_FUNDS, "Cannot gather enough funds.");
             }
 
