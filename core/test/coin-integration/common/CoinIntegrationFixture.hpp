5c97103088be8725a72a1c36ec370a7a4f145b07
huiqi <huiqi.zheng@ledger.fr>
2020-10-03 21:49:40 +0200

    fix build error

--- core/test/coin-integration/common/CoinIntegrationFixture.hpp
+++ core/test/coin-integration/common/CoinIntegrationFixture.hpp
@@ -48,7 +48,6 @@
 #include <wallet/pool/database/PoolDatabaseHelper.hpp>
 #include <utils/JSONUtils.h>
 #include <wallet/bitcoin/explorers/api/TransactionParser.hpp>
-#include <async/async_wait.h>
 #include <wallet/bitcoin/BitcoinLikeAccount.hpp>
 #include <wallet/ethereum/EthereumLikeAccount.h>
 #include <api/BitcoinLikeOperation.hpp>
@@ -139,7 +138,7 @@ public:
                                                                  const api::ExtendedKeyAccountCreationInfo& info) {
         auto i = info;
         i.index = index;
-        return std::dynamic_pointer_cast<Account>(::wait(wallet->newAccountWithExtendedKeyInfo(i)));
+        return std::dynamic_pointer_cast<Account>(uv::wait(wallet->newAccountWithExtendedKeyInfo(i)));
     }
 
 
