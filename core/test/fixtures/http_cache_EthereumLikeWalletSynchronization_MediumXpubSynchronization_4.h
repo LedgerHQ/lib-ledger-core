a2e56ab51f1d3149f8bd9631b5b9222c3be5663f
Habib LAFET <habib.lafet-ext@ledger.fr>
2020-10-07 18:59:11 +0200

    fix more test errors

---
+++ core/test/fixtures/http_cache_EthereumLikeWalletSynchronization_MediumXpubSynchronization_4.h
@@ -0,0 +1,4 @@
+namespace HTTP_CACHE_EthereumLikeWalletSynchronization_MediumXpubSynchronization_4 {
+const std::string URL = R"(https://explorers.api.live.ledger.com/blockchain/v3/eth/addresses/0xabf06640f8ca8fC5e0Ed471b10BeFCDf65A33e43/balance)";
+const std::string BODY= R"([{"address":"0xabf06640f8ca8fC5e0Ed471b10BeFCDf65A33e43","balance":8598521241423646,"valid":true,"error":false}])";
+}
