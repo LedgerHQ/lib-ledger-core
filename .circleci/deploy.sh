d9c20e9642317e47edde440a5b3b5018c4ed8251
root <habib.lafet-ext@ledger.fr>
2020-10-27 17:41:54 +0100

    build artifacts for test

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
-if [ -n "$CIRCLE_TAG" ] || [ "$CIRCLE_BRANCH" == "master" -o "$CIRCLE_BRANCH" == "develop" ] || [ "$CIRCLE_BRANCH" == "nrt" ] ; then
-	cd ../lib-ledger-core-artifacts
+#if [ -n "$CIRCLE_TAG" ] || [ "$CIRCLE_BRANCH" == "master" -o "$CIRCLE_BRANCH" == "develop" ]; then	cd ../lib-ledger-core-artifacts
 	ls -la
 	aws s3 sync ./ s3://ledger-lib-ledger-core/$LIB_VERSION/ --acl public-read && \
 	aws s3 ls s3://ledger-lib-ledger-core/$LIB_VERSION;
 	cd -
-fi
+#fi
--- .circleci/export_lib_version.sh
+++ .circleci/export_lib_version.sh
@@ -11,7 +11,7 @@ function match {
 LIB_VERSION_MAJOR=`cat CMakeLists.txt | match 's/set\(VERSION_MAJOR.*([0-9]+).*\)/\1/p'`
 LIB_VERSION_MINOR=`cat CMakeLists.txt | match 's/set\(VERSION_MINOR.*([0-9]+).*\)/\1/p'`
 LIB_VERSION_PATCH=`cat CMakeLists.txt | match 's/set\(VERSION_PATCH.*([0-9]+).*\)/\1/p'`
-LIB_VERSION=$LIB_VERSION_MAJOR.$LIB_VERSION_MINOR.$LIB_VERSION_PATCH
+LIB_VERSION=$LIB_VERSION_MAJOR.$LIB_VERSION_MINOR.$LIB_VERSION_PATCH.tezos
 
 if [ -z "$CIRCLE_TAG" ]; then
 	COMMIT_HASH=`echo $CIRCLE_SHA1 | cut -c 1-6`
