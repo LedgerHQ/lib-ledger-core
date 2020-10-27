a59f2ec1a2f0f83436d8a5db08977d73a2e6ef39
root <habib.lafet-ext@ledger.fr>
2020-10-27 17:41:54 +0100

    build artifacts for test

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-if [ -n "$CIRCLE_TAG" ] || [ "$CIRCLE_BRANCH" == "master" -o "$CIRCLE_BRANCH" == "develop" ]; then
+#if [ -n "$CIRCLE_TAG" ] || [ "$CIRCLE_BRANCH" == "master" -o "$CIRCLE_BRANCH" == "develop" ]; then
 	cd ../lib-ledger-core-artifacts
 	ls -la
 	aws s3 sync ./ s3://ledger-lib-ledger-core/$LIB_VERSION/ --acl public-read && \
 	aws s3 ls s3://ledger-lib-ledger-core/$LIB_VERSION;
 	cd -
-fi
+#fi
