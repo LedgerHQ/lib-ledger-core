1f2d1130b6f56323cef457074e30f4cd8400b104
Gerry Agbobada <gerry.agbobada-ext@ledger.fr>
2021-02-18 17:00:19 +0100

    Another fixme

--- core/src/wallet/currencies.cpp
+++ core/src/wallet/currencies.cpp
@@ -247,8 +247,7 @@ namespace ledger {
             // https://github.com/cosmos/gaia/blob/ba8d2b3177e1b891b72d6f40538fc2c6344bdeac/docs/delegators/delegator-guide-cli.md#sending-transactions
             const api::Currency ATOM =
                 Currency("cosmos")
-                 /// FIXME: Bump this chain for Stargate release on mainnet in production
-                .forkOfCosmos(networks::getCosmosLikeNetworkParameters("cosmoshub-3"))
+                .forkOfCosmos(networks::getCosmosLikeNetworkParameters("cosmoshub-4"))
                 .bip44(ATOM_COIN_ID)
                 .paymentUri("cosmos")
                 .unit("uatom", 0, "uatom")
