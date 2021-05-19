983d18ebe480b38cca5b78a9dac68d973368b98e
tongkunvwang <grey19860000@gmail.com>
2021-05-19 10:09:42 +0200

    publish artifact for live_4.2 branch

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-if [ -n "$CIRCLE_TAG" ] || [ "$CIRCLE_BRANCH" == "master" -o "$CIRCLE_BRANCH" == "develop" ]; then
+if [ -n "$CIRCLE_TAG" ] || [ "$CIRCLE_BRANCH" == "master" -o "$CIRCLE_BRANCH" == "develop" ] || [ "$CIRCLE_BRANCH" == "live_4.2" ]; then
 	cd ../lib-ledger-core-artifacts
 	ls -la
 	aws s3 sync ./ s3://ledger-lib-ledger-core/$LIB_VERSION/ --acl public-read && \
