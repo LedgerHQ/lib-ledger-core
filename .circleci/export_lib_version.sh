970a8bba55877d9f2dcb23b3ffc6c1ec21a0df04
Thomas Wilgenbus <thomas.wilgenbus-ext@ledger.fr>
2021-08-18 16:14:20 +0200

    Allow for export_lib_version.sh to be called outside of CCI

--- .circleci/export_lib_version.sh
+++ .circleci/export_lib_version.sh
@@ -8,17 +8,19 @@ function match {
   fi
 }
 
-LIB_VERSION_MAJOR=`cat CMakeLists.txt | match 's/set\(VERSION_MAJOR.*([0-9]+).*\)/\1/p'`
-LIB_VERSION_MINOR=`cat CMakeLists.txt | match 's/set\(VERSION_MINOR.*([0-9]+).*\)/\1/p'`
-LIB_VERSION_PATCH=`cat CMakeLists.txt | match 's/set\(VERSION_PATCH.*([0-9]+).*\)/\1/p'`
+LIB_VERSION_MAJOR=$(cat CMakeLists.txt | match 's/set\(VERSION_MAJOR.*([0-9]+).*\)/\1/p')
+LIB_VERSION_MINOR=$(cat CMakeLists.txt | match 's/set\(VERSION_MINOR.*([0-9]+).*\)/\1/p')
+LIB_VERSION_PATCH=$(cat CMakeLists.txt | match 's/set\(VERSION_PATCH.*([0-9]+).*\)/\1/p')
 LIB_VERSION=$LIB_VERSION_MAJOR.$LIB_VERSION_MINOR.$LIB_VERSION_PATCH
 
-if [ -z "$CIRCLE_TAG" ]; then
-	COMMIT_HASH=`echo $CIRCLE_SHA1 | cut -c 1-6`
-	LIB_VERSION="$LIB_VERSION-rc-$COMMIT_HASH"
+if [[ -z "$CIRCLE_TAG" ]]; then
+  if [[ -n "$CIRCLE_SHA1" ]]; then
+    COMMIT_HASH=$(echo "$CIRCLE_SHA1" | cut -c 1-6)
+  else
+    COMMIT_HASH=$(git rev-parse --short HEAD | cut -c 1-6)
+  fi
+  LIB_VERSION="$LIB_VERSION-rc-$COMMIT_HASH"
 fi
 
-echo "export LIB_VERSION=$LIB_VERSION" >> $BASH_ENV
-
-echo "=====> Libcore version"
-echo $LIB_VERSION 
+echo "export LIB_VERSION=$LIB_VERSION" >> $"BASH_ENV"
+echo "$LIB_VERSION"
