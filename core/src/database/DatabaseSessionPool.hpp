745a0a9fbb494aa4ec7480c7e41b606fca6deaf0
Jérémy Coatelen <jeremy.coatelen@ledger.fr>
2021-12-03 17:52:50 +0100

    fix: very long delete operations on sql db for bitcoin tx
    The fix consists in creating specific indexes in the sql db to accelerate on delete cascade for bitcoin transactions.

--- core/src/database/DatabaseSessionPool.hpp
+++ core/src/database/DatabaseSessionPool.hpp
@@ -61,7 +61,7 @@ namespace ledger {
                 const std::string &password = ""
             );
 
-            static const int CURRENT_DATABASE_SCHEME_VERSION = 27;
+            static const int CURRENT_DATABASE_SCHEME_VERSION = 28;
 
             void performDatabaseMigration();
             void performDatabaseRollback();
