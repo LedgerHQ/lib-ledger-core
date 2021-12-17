90c64dedb5af0c91e4921c583c9bce8178eb2c45
Viktor Bocharov <94441294+viktorb-ledger@users.noreply.github.com>
2021-12-17 14:29:58 +0100

    VG-2538: publish libcore jar on S3

--- .circleci/publish_jar.sh
+++ .circleci/publish_jar.sh
@@ -6,6 +6,8 @@ JAVA_API_DIR=api/core/java
 SCALA_API_DIR=api/core/scala
 RESOURCE_DIR=$JAR_BUILD_DIR/src/main/resources/resources/djinni_native_libs
 
+bash tools/generate_interfaces.sh
+
 rm -rf $JAR_BUILD_DIR
 mkdir $JAR_BUILD_DIR
 mkdir -p $RESOURCE_DIR
@@ -17,12 +19,13 @@ cp $BIN_DIR/macos/jni/libledger-core_jni.dylib $RESOURCE_DIR
 cp .circleci/build.sbt $JAR_BUILD_DIR
 
 cd $JAR_BUILD_DIR
-if [ -n "$CIRCLE_TAG" ] || [ "$CIRCLE_BRANCH" == "master" -o "$CIRCLE_BRANCH" == "develop" ]; then
+if [ -n "$CIRCLE_TAG" ] || [ "$CIRCLE_BRANCH" == "master" -o "$CIRCLE_BRANCH" == "develop" ] || [[ "$CIRCLE_BRANCH" == "release/"* ]] ; then
 	if [[ $LIB_VERSION == *"-rc-"* ]]; then
 		export JAR_VERSION="$LIB_VERSION"-SNAPSHOT
 	else
 		export JAR_VERSION=$LIB_VERSION
 	fi
-	JAR_VERSION=$JAR_VERSION sbt publish
+  JAR_VERSION=$JAR_VERSION sbt package
+  cp ./target/scala-2.12/scala-lib-core_2.12-${JAR_VERSION}.jar ~/lib-ledger-core-artifacts/ledger-lib-core.jar
 fi
 
