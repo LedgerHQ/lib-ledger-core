27550623ad089cc35ccbb94f972faf5044c016ec
Viktor Bocharov <94441294+viktorb-ledger@users.noreply.github.com>
2022-01-03 17:16:45 +0100

    Simplify libcore version generation

--- nix/scripts/export_libcore_version.sh
+++ nix/scripts/export_libcore_version.sh
@@ -13,23 +13,20 @@ LIB_VERSION=$LIB_VERSION_MAJOR.$LIB_VERSION_MINOR.$LIB_VERSION_PATCH
 
 COMMIT_HASH=`echo $GITHUB_SHA | cut -c 1-6`
 LIBCORE_VERSION="$LIB_VERSION-rc-$COMMIT_HASH"
-LIBCORE_GIT_DESCRIBE=`git describe --tags`
+LIBCORE_GIT_DESCRIBE=`git describe --tags --first-parent`
 
 echo "=====> Libcore version : $LIBCORE_VERSION"
-#https://docs.github.com/en/actions/reference/workflow-commands-for-github-actions#setting-an-output-parameter
+# https://docs.github.com/en/actions/reference/workflow-commands-for-github-actions#setting-an-output-parameter
 echo "::set-output name=lib_version::$LIBCORE_VERSION"
 
 printf "# github event is **${GITHUB_EVENT_NAME}**\n"
-if [[ "${GITHUB_EVENT_NAME:-NO}" == "release" ]]; then
+if [[ "${GITHUB_EVENT_NAME}" == "release" ]]; then
     printf "\nMarking for deploy\n"
     echo "::set-output name=deploy_dynlibs::YES"
     echo "::set-output name=jar_version::${LIB_VERSION}"
-elif [[ "${GITHUB_EVENT_NAME:-NO}" == "push" ]]; then
-    printf "\n${GITHUB_EVENT_NAME:-NO} is \"push\", creating a snapshot\n"
-    echo "::set-output name=deploy_dynlibs::NO"
-    echo "::set-output name=jar_version::${LIB_VERSION}-${LIBCORE_GIT_DESCRIBE}-SNAPSHOT"
-elif [[ "${GITHUB_EVENT_NAME:-NO}" == "pull_request" ]]; then
-    printf "\n${GITHUB_EVENT_NAME:-NO} is \"pull_request\", creating a snapshot\n"
+elif [[ "${GITHUB_EVENT_NAME}" == "push" ]] || [[ "${GITHUB_EVENT_NAME}" == "pull_request" ]] ; then
+    printf "\nMarking for snapshot\n"
     echo "::set-output name=deploy_dynlibs::NO"
     echo "::set-output name=jar_version::${LIB_VERSION}-${LIBCORE_GIT_DESCRIBE}-SNAPSHOT"
 fi
+
