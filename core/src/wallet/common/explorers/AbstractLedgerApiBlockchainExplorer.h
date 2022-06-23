442c5decc257b1a7a91735ea17d4b7dc3eaf5c05
Bertrand Darbon <bertrand.darbon@ledger.fr>
2022-06-23 09:16:29 +0200

    VG-2096 support batch size argument in when getting transactions list from explorers (#882)
    * VG-2096 Support batch_size argument in when getting transactions list from explorers
    
    * When getting exactly 1k transaction, checks if first and last block hash are identical.
    * If so, we might be in the edge case where there is more than 1k transaction in this block for this address.
    
    * update http cache files
    
    * fix when no transactions
    
    * reduce complexity of getLedgerApiTransactions
    
    fix mac compilation ?
    
    fix bad optional access
    
    * update stellar unit test after testnet reset

--- core/src/wallet/common/explorers/AbstractLedgerApiBlockchainExplorer.h
+++ core/src/wallet/common/explorers/AbstractLedgerApiBlockchainExplorer.h
@@ -49,33 +49,59 @@ namespace ledger {
         //TODO: remove TransactionsParser and TransactionsBulkParser from template when refactoring them (common interface)
         template <typename BlockchainExplorerTransaction, typename TransactionsBulk, typename TransactionsParser, typename TransactionsBulkParser, typename BlockParser, typename NetworkParameters>
         class AbstractLedgerApiBlockchainExplorer {
+          constexpr static const uint16_t DEFAULT_BATCH_SIZE = 1000;
+
+        private:
+          std::string getParams(Option<std::string> fromBlockHash,
+                                const Option<void *>& session,
+                                bool isSnakeCase = false,
+                                uint16_t batch_size = DEFAULT_BATCH_SIZE) {
+            std::string params;
+            std::unordered_map<std::string, std::string> headers;
+
+            if (session.isEmpty()) {
+              params = isSnakeCase ? "?no_token=true" : "?noToken=true";
+            }
+
+            // Block hash
+            if (!fromBlockHash.isEmpty()) {
+              if (!params.empty()) {
+                params = params + "&";
+              } else {
+                params = params + "?";
+              }
+              const std::string blockHash = isSnakeCase ? "block_hash=" : "blockHash=";
+              params = params + blockHash + fromBlockHash.getValue();
+            }
+
+            // Batch size
+            if (!params.empty()) {
+              params = params + "&";
+            } else {
+              params = params + "?";
+            }
+            params = params + "batch_size=" + std::to_string(batch_size);
+            return params;
+          }
+
         public:
             FuturePtr<TransactionsBulk>
             getLedgerApiTransactions(const std::vector<std::string> &addresses,
                             Option<std::string> fromBlockHash,
                             Option<void *> session,
-                            bool isSnakeCase = false) {
+                            bool isSnakeCase = false,
+                            uint16_t batch_size = DEFAULT_BATCH_SIZE) {
                 auto joinedAddresses = Array<std::string>(addresses).join(strings::mkString(",")).getValueOr("");
-                std::string params;
+                std::string params = getParams(fromBlockHash, session, isSnakeCase, batch_size);
                 std::unordered_map<std::string, std::string> headers;
 
-                if (session.isEmpty()) {
-                    params = isSnakeCase ? "?no_token=true" : "?noToken=true";
-                } else {
+                if (!session.isEmpty()) {
                     headers["X-LedgerWallet-SyncToken"] = *((std::string *)session.getValue());
                 }
-                if (!fromBlockHash.isEmpty()) {
-                    if (params.size() > 0) {
-                        params = params + "&";
-                    } else {
-                        params = params + "?";
-                    }
-                    auto blockHash = isSnakeCase ? "block_hash=" : "blockHash=";
-                    params = params + blockHash + fromBlockHash.getValue();
-                }
+
                 return _http->GET(fmt::format("/blockchain/{}/{}/addresses/{}/transactions{}", getExplorerVersion(), getNetworkParameters().Identifier, joinedAddresses, params), headers)
                         .template json<TransactionsBulk, Exception>(LedgerApiParser<TransactionsBulk, TransactionsBulkParser>(), true)
-                        .template mapPtr<TransactionsBulk>(getExplorerContext(), [fromBlockHash] (const Either<Exception, std::shared_ptr<TransactionsBulk>>& result) {
+                        .template mapPtr<TransactionsBulk>(getExplorerContext(), [fromBlockHash, batch_size] (const Either<Exception, std::shared_ptr<TransactionsBulk>>& result) {
                             if (result.isLeft()) {
                                 // Only case where we should emit block not found error
                                 if (!fromBlockHash.isEmpty() && result.getLeft().getErrorCode() == api::ErrorCode::HTTP_ERROR) {
@@ -83,9 +109,25 @@ namespace ledger {
                                 } else {
                                     throw result.getLeft();
                                 }
-                            } else {
-                                return result.getRight();
                             }
+
+                            auto bulk = result.getRight();
+                            if (bulk->transactions.empty() || !bulk->transactions.front().block.hasValue() || !bulk->transactions.back().block.hasValue()) {
+                              return bulk;
+                            }
+                            auto firstBlock = bulk->transactions.front().block.getValue();
+                            auto lastBlock = bulk->transactions.back().block.getValue();
+                            if (bulk->transactions.size() == batch_size && firstBlock.hash == lastBlock.hash)
+                            { // We might have more than {batch_size} transactions in a single block for this address, let's ask for more transaction ! (recovered exception below)
+                              throw make_exception(api::ErrorCode::INCOMPLETE_TRANSACTION, "Some transaction might be missing !");
+                            }
+                            return result.getRight();
+                        })
+                        .recoverWith(getExplorerContext(), [this, addresses, fromBlockHash, session, isSnakeCase, batch_size](const Exception&e) {
+                          if (e.getErrorCode() != api::ErrorCode::INCOMPLETE_TRANSACTION) {
+                            throw e;
+                          }
+                          return getLedgerApiTransactions(addresses, fromBlockHash, session, isSnakeCase, batch_size + DEFAULT_BATCH_SIZE);
                         });
             };
 
