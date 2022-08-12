79929c83c19b3f3ccd694e428a8a71188ebdcaf7
Viktor Bocharov <94441294+viktorb-ledger@users.noreply.github.com>
2022-08-12 16:30:17 +0200

    Build from sources in the current directory

--- tools/prod-like-build-inner.sh
+++ tools/prod-like-build-inner.sh
@@ -7,17 +7,23 @@ then
   BUILD_TYPE=$1
 fi
 
-cd ~
-git clone --recursive https://github.com/LedgerHQ/lib-ledger-core.git
+cp -r ~/lib-ledger-core-ro ~/lib-ledger-core
+rm -rf ~/lib-ledger-core/djinni/src/target/ || echo "Dir \"djinni/src/target/\" doesn't exists (that's ok)."
+
 export CORE_SSL_1_1=arch_ssl_1_1
+
 mkdir -p ~/lib-ledger-core-artifacts
 cd ~/lib-ledger-core
+
 .circleci/build_lib.sh target_jni $BUILD_TYPE
+
 export LIB_VERSION=`.circleci/export_lib_version.sh`
+
 . .circleci/copy_artifacts.sh target_jni
 
 . tools/generate_interfaces.sh
 
+# We don't try to build for MacOS here
 mkdir -p ~/lib-ledger-core-artifacts/macos/jni
 touch ~/lib-ledger-core-artifacts/macos/jni/libledger-core_jni.dylib
 
--- tools/prod-like-build.sh
+++ tools/prod-like-build.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
+BUILD_TYPE=$1
+
 if [ ! -d lib-ledger-core-build-env ]
 then
   git clone git@github.com:LedgerHQ/lib-ledger-core-build-env.git
@@ -9,14 +11,16 @@ else
   git pull
   cd ..
 fi
-cp tools/prod-like-build-inner.sh lib-ledger-core-build-env/scripts/
+
 cd lib-ledger-core-build-env
-git checkout update-installation
 docker build -t lib-ledger-core-build-env:latest .
 cd ..
+
 mkdir -p artifacts
 docker run \
     --mount type=bind,source="$(pwd)"/artifacts,target=/hostdir \
+    --mount type=bind,source="$(pwd)"/tools,target=/root/tools \
+    --mount type=bind,source="$(pwd)",target=/root/lib-ledger-core-ro,readonly \
     lib-ledger-core-build-env:latest \
-    /scripts/prod-like-build-inner.sh Debug
+    /root/tools/prod-like-build-inner.sh ${BUILD_TYPE}
 
