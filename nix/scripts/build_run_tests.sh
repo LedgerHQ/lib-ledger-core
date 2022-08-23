0a17f0d238df3350983b4a6ecb7a1378b2518c52
Bertrand Darbon <bertrand.darbon@ledger.fr>
2022-08-23 14:45:31 +0200

    Use ninja

--- nix/scripts/build_run_tests.sh
+++ nix/scripts/build_run_tests.sh
@@ -4,13 +4,15 @@ set -euo pipefail
 LOAD_LIMIT="${BUILD_LOAD_LIMIT:-2.5}"
 echo "LOAD_LIMIT==" ${LOAD_LIMIT}
 
+apt-get update && apt-get install ninja-build  # TODO: move it to build env
+
 mkdir _build_tests
 cd _build_tests
-cmake .. -DBUILD_TESTS=ON -DNIX_BUILD=OFF -DCCACHE=ON
+cmake .. -DBUILD_TESTS=ON -DNIX_BUILD=OFF -DCCACHE=ON -GNinja
 echo "========= Building libcore"
-make -j -l"${LOAD_LIMIT}" ledger-core-static
-make -j -l"${LOAD_LIMIT}" ledger-core
-make -j -l"${LOAD_LIMIT}"
+cmake --build . --parallel --target  ledger-core-static
+cmake --build . --parallel --target  ledger-core
+cmake --build . --parallel
 echo "========= Provisioning Database"
 initdb -D .tmp/mydb
 pg_ctl -D .tmp/mydb -l logfile -o "--unix_socket_directories='$PWD'" start
