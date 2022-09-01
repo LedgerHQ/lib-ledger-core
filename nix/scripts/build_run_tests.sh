4cb164e5ada2ea351b462e2b358ffc1a3957073b
Bertrand Darbon <bertrand.darbon@ledger.fr>
2022-09-01 18:27:13 +0200

    Rollback to pg 13

--- nix/scripts/build_run_tests.sh
+++ nix/scripts/build_run_tests.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 mkdir _build_tests
 cd _build_tests
-cmake .. -DBUILD_TESTS=ON -DNIX_BUILD=OFF -DCCACHE=ON -DSYS_OPENSSL=ON -DOPENSSL_USE_STATIC_LIBS=TRUE -DPOSTGRES_HOST=$POSTGRES_HOST -DPOSTGRES_PORT=$POSTGRES_PORT -GNinja $1
+cmake .. -DBUILD_TESTS=ON -DNIX_BUILD=OFF -DCCACHE=ON -DSYS_OPENSSL=ON -DOPENSSL_USE_STATIC_LIBS=TRUE -DPOSTGRES_HOST=$POSTGRES_HOST -DPOSTGRES_PORT=$POSTGRES_PORT -GNinja
 echo "========= Building libcore"
 cmake --build . --parallel --target  ledger-core-static
 cmake --build . --parallel --target  ledger-core
