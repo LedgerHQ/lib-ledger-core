17bbb8b5001218f22a9f3bb73b49fd9935aa91d0
Bertrand Darbon <bertrand.darbon@ledger.fr>
2022-09-01 12:00:36 +0200

    Add macos build

--- nix/scripts/build_run_tests.sh
+++ nix/scripts/build_run_tests.sh
@@ -1,11 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-LOAD_LIMIT="${BUILD_LOAD_LIMIT:-2.5}"
-echo "LOAD_LIMIT==" ${LOAD_LIMIT}
-
-apt-get update && apt-get install ninja-build  # TODO: move it to build env
-
 mkdir _build_tests
 cd _build_tests
 cmake .. -DBUILD_TESTS=ON -DNIX_BUILD=OFF -DCCACHE=ON -DSYS_OPENSSL=ON -DOPENSSL_USE_STATIC_LIBS=TRUE -DPOSTGRES_HOST=$POSTGRES_HOST -DPOSTGRES_PORT=$POSTGRES_PORT -GNinja
