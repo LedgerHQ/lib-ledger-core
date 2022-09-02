49b6e1ee053ee73a1985a37209dc00293d49da6a
Bertrand Darbon <bertrand.darbon@ledger.fr>
2022-09-02 20:16:08 +0200

    fix checkout

--- .github/scripts/publish_to_wd.sh
+++ .github/scripts/publish_to_wd.sh
@@ -3,8 +3,7 @@ set -euo pipefail
 
 # Update libcore submodule
 cd $GITHUB_WORKSPACE/ledger-wallet-daemon/libcore/lib-ledger-core
-git fetch
-git checkout $GITHUB_REF
+git checkout ${GITHUB_REF#refs/heads/}
 
 # Commit & push changes
 cd $GITHUB_WORKSPACE/ledger-wallet-daemon
