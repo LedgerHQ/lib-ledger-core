9840d93132adc0010c1ce2042e6860975b080e18
Bertrand Darbon <bertrand.darbon@ledger.fr>
2022-09-02 06:35:12 +0200

    Revert "don't use sys openssl on macos"
    This reverts commit 6cc934f5f54f9eeb8c5bf12cfce128f2201deffb.

--- nix/scripts/build_run_tests.sh
+++ nix/scripts/build_run_tests.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 mkdir _build_tests
 cd _build_tests
-cmake .. -DBUILD_TESTS=ON -DNIX_BUILD=OFF -DCCACHE=ON -DSYS_OPENSSL=${SYS_OPENSSL-ON} -DOPENSSL_USE_STATIC_LIBS=${OPENSSL_USE_STATIC_LIBS-TRUE} -DPOSTGRES_HOST=$POSTGRES_HOST -DPOSTGRES_PORT=$POSTGRES_PORT -GNinja
+cmake .. -DBUILD_TESTS=ON -DNIX_BUILD=OFF -DCCACHE=ON -DSYS_OPENSSL=ON -DOPENSSL_USE_STATIC_LIBS=TRUE -DPOSTGRES_HOST=$POSTGRES_HOST -DPOSTGRES_PORT=$POSTGRES_PORT -GNinja
 echo "========= Building libcore"
 cmake --build . --parallel --target  ledger-core-static
 cmake --build . --parallel --target  ledger-core
