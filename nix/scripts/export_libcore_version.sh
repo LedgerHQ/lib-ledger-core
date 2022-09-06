74d9b113db6ec90eec2d455b13751590ebf0df8f
Bertrand Darbon <bertrand.darbon@ledger.fr>
2022-09-06 14:21:01 +0200

    print for debug

--- nix/scripts/export_libcore_version.sh
+++ nix/scripts/export_libcore_version.sh
@@ -29,6 +29,7 @@ else
     echo "Base ref name: "${GITHUB_BASE_REF:=main}
     BRANCH_LENGTH=`git rev-list --count ^remotes/origin/${GITHUB_BASE_REF:=main} remotes/origin/${BRANCH_NAME}`
     echo "Branch length: " $BRANCH_LENGTH
+    git rev-list ^remotes/origin/${GITHUB_BASE_REF:=main} remotes/origin/${BRANCH_NAME}
     ABBREV_COMMIT_HASH=`git rev-list ^remotes/origin/${GITHUB_BASE_REF:=main} remotes/origin/${BRANCH_NAME} | head -n 1 | cut -c 1-6`
     echo "Commit hash: " $ABBREV_COMMIT_HASH
     LIBCORE_VERSION=${LIB_VERSION}-${BRANCH_NAME}-${BRANCH_LENGTH}-${ABBREV_COMMIT_HASH}
