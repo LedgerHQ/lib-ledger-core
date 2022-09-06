d46d8c0cc3bd640a91fa7bce0ba3aa81057269cc
Bertrand Darbon <bertrand.darbon@ledger.fr>
2022-09-06 12:03:43 +0200

    try to build in release instead of debug

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 mkdir _build_tests
 cd _build_tests
-cmake .. -DCMAKE_BUILD_TYPE=${BUILD_TYPE-Debug} -DTARGET_JNI=${BUILD_JNI-OFF} -DBUILD_TESTS=ON -DNIX_BUILD=OFF -DCCACHE=ON -DSYS_OPENSSL=ON -DOPENSSL_USE_STATIC_LIBS=TRUE -GNinja
+cmake .. -DCMAKE_BUILD_TYPE=${BUILD_TYPE-Release} -DTARGET_JNI=${BUILD_JNI-OFF} -DBUILD_TESTS=ON -DNIX_BUILD=OFF -DCCACHE=ON -DSYS_OPENSSL=ON -DOPENSSL_USE_STATIC_LIBS=TRUE -GNinja
 echo "========= Building libcore"
 cmake --build . --parallel --target  ledger-core-static
 cmake --build . --parallel --target  ledger-core
