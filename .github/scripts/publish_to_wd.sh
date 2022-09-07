1432199d930fdd65fcb4637348c0083e492dc124
Bertrand Darbon <bertrand.darbon@ledger.fr>
2022-09-07 09:38:20 +0200

    Create PR with bump of jar version

--- .github/scripts/publish_to_wd.sh
+++ .github/scripts/publish_to_wd.sh
@@ -1,22 +1,18 @@
 #!/usr/bin/env bash
 set -euo pipefail
+cd $GITHUB_WORKSPACE/ledger-wallet-daemon
+sed -i -E 's/(val libcore *= *\")(.*)\"/\1'$JAR_VERSION'\"/' build.sbt
 
-# Update libcore submodule
-#cd $GITHUB_WORKSPACE/ledger-wallet-daemon/libcore/lib-ledger-core
-#git remote -v
-#pwd
-#ls -la
-#git checkout ${GITHUB_REF#refs/heads/}
+# Bump libcore version
 
 # Commit & push changes
-cd $GITHUB_WORKSPACE/ledger-wallet-daemon
-git checkout -b bump_libcore_$VERSION
+git checkout -b bump_libcore_$LIB_VERSION
 git add lib # libcore
 git config --global user.name "${GITHUB_ACTOR}"
 git config --global user.email "${GITHUB_ACTOR}@users.noreply.github.com"
-git commit -m "Bump libcore to $VERSION"
-git push --set-upstream origin bump_libcore_$VERSION
-gh pr create --title "Bump libcore to $VERSION" \
+git commit -m "Bump libcore to $LIB_VERSION"
+git push --set-upstream origin bump_libcore_$LIB_VERSION
+gh pr create --title "Bump libcore to $LIB_VERSION" \
    --body "Automatic update from libcore release" \
    --base main \
-   --head bump_libcore_$VERSION
+   --head bump_libcore_$LIB_VERSION
