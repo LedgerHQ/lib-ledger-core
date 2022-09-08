eb432e2eddb8af868bf8e0139622ac3722d8d80f
Bertrand Darbon <bertrand.darbon@ledger.fr>
2022-09-08 11:11:54 +0200

    Rollback unused changes

--- nix/scripts/build_run_tests.sh
+++ nix/scripts/build_run_tests.sh
@@ -1,12 +1,19 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+LOAD_LIMIT="${BUILD_LOAD_LIMIT:-2.5}"
+echo "LOAD_LIMIT==" ${LOAD_LIMIT}
+
 mkdir _build_tests
 cd _build_tests
-cmake .. -DBUILD_TESTS=ON -DNIX_BUILD=OFF -DCCACHE=ON -DSYS_OPENSSL=ON -DOPENSSL_USE_STATIC_LIBS=TRUE -DPOSTGRES_HOST=$POSTGRES_HOST -DPOSTGRES_PORT=$POSTGRES_PORT -GNinja
+cmake .. -DBUILD_TESTS=ON -DNIX_BUILD=ON
 echo "========= Building libcore"
-cmake --build . --parallel --target  ledger-core-static
-cmake --build . --parallel --target  ledger-core
-cmake --build . --parallel
+make -j -l"${LOAD_LIMIT}" ledger-core-static
+make -j -l"${LOAD_LIMIT}" ledger-core
+make -j -l"${LOAD_LIMIT}"
+echo "========= Provisioning Database"
+initdb -D .tmp/mydb
+pg_ctl -D .tmp/mydb -l logfile -o "--unix_socket_directories='$PWD'" start
+createdb -p 5432 -h localhost test_db
 echo "========= Running tests"
 ctest --output-on-failure
