cd9b8581aaa224fa814b3d242eed0274ea993d7d
Bertrand Darbon <bertrand.darbon@ledger.fr>
2022-09-16 11:21:18 +0200

    Fix release workflow (#907)

--- .github/scripts/publish_to_wd.sh
+++ .github/scripts/publish_to_wd.sh
@@ -8,7 +8,7 @@ sed -i -E 's/(val libcore *= *\")(.*)\"/\1'$VERSION'\"/' build.sbt
 
 # Commit & push changes
 git checkout -b bump_libcore_$VERSION
-git add lib # libcore
+git add build.sbt
 git config --global user.name "${GITHUB_ACTOR}"
 git config --global user.email "${GITHUB_ACTOR}@users.noreply.github.com"
 git commit -m "Bump libcore to $VERSION"
