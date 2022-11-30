7079d5dd2d93040d21c11e18bf0e2a3e3a0b4832
Bertrand Darbon <bertrand.darbon@ledger.fr>
2022-11-30 13:43:47 +0100

    Fix macos build

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -3,8 +3,8 @@ set -euo pipefail
 
 mkdir _build_tests
 cd _build_tests
-cmake .. -DTARGET_JNI=${BUILD_JNI-OFF} -DBUILD_TESTS=ON -DNIX_BUILD=OFF -DCCACHE=ON -DSYS_OPENSSL=ON -DOPENSSL_USE_STATIC_LIBS=TRUE -GNinja
+cmake .. -DTARGET_JNI=${BUILD_JNI-OFF} -DBUILD_TESTS=ON -DNIX_BUILD=OFF -DCCACHE=ON -DSYS_OPENSSL=ON -DOPENSSL_USE_STATIC_LIBS=TRUE ${ARGS}
 echo "========= Building libcore"
-cmake --build . --parallel --target  ledger-core-static
-cmake --build . --parallel --target  ledger-core
-cmake --build . --parallel
+cmake --build . --target  ledger-core-static -- -j2
+cmake --build . --target  ledger-core -- -j2
+cmake --build . -- -j2
